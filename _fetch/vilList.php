cfc586c418284d56ae83c7494f9e094d5d1bce44
fortmorst <fortmorst@server.fake>
2013-09-13 18:58:47 +0900

    人参国以外にも対応出来るよう書き直し

--- _fetch/vilList.php
+++ _fetch/vilList.php
@@ -1,80 +1,103 @@
 <?
 require_once('simple_html_dom.php');
 
-//取得する国のアルファベットを入力;
-$c = 'old2';
+define('N_OLD1','1');
+define('N_MAIN','2');
+define('N_A','3');
+define('N_B','4');
+define('N_C','5');
+define('N_D','6');
+define('N_E','7');
+define('N_F','8');
+define('N_G','9');
+define('N_OLD2','10');
+define('GUTA','11');
+
+$URL_LIST = array(
+  "",
+  "http://ninjinix.x0.com/wolf_old/",
+  "http://ninjinix.x0.com/wolf0/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfa/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfb/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfc/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfd/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfe/index.rb?cmd=log",
+  "http://ninjin002.x0.com/wolff/index.rb?cmd=log",
+  "http://wolfg.x0.com/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolf/",
+  "http://www3.marimo.or.jp/~fgmaster/cabala/sow.cgi?cmd=oldlog"
+);
 
 
-/*--------------------------------------------------------------------------------
-
-  0. 定数・変数
-
-  --------------------------------------------------------------------------------*/
-
-define('URL_OLD','http://ninjinix.x0.com/wolf');
-define('URL_LOG','/index.rb?cmd=log');
-define('URL_F','http://ninjin002.x0.com/wolff');
-define('URL_G','http://www.wolfg.x0.com');
-
-switch($c)
-{
-  case 'a':
-  case 'b':
-  case 'c':
-  case 'd':
-  case 'e':
-  case '0':
-    $url_list = URL_OLD.$c.URL_LOG;
-    $url_vil = URL_OLD.$c.'/';
-    break;
-  case 'old2':
-    $url_list = URL_OLD.'/';
-    $url_vil = URL_OLD.'/';
-    break;
-  case 'f';
-    $url_list = URL_F.URL_LOG;
-    $url_vil = URL_F.'/';
-    break;
-  case 'g';
-    $url_list = URL_G.URL_LOG;
-    $url_vil  = URL_G.'/';
-    break;
-}
-
-
-// 村リスト一覧取得テスト
 $html = new simple_html_dom();
-$html->load_file($url_list);
+$country = $argv[1];
+$url = $URL_LIST[$country];
+$html->load_file($url);
 
-//村IDと村名を取得
-if ($c === 'old2')
+switch($country)
 {
-  $rawVilList = $html->find('table.list',0)->find('a');
-} else {
-  $rawVilList = $html->find('div.main a');
-  // 最後のリンク(トップに戻るを削除
-  array_pop($rawVilList);
+  case N_OLD1:
+  case N_OLD2:
+    $vil_list = $html->find('table.list',0)->find('a');
+    break;
+  case N_MAIN:
+  case N_A:
+  case N_B:
+  case N_C:
+  case N_D:
+  case N_E:
+  case N_F:
+  case N_G:
+    $vil_list = $html->find('div.main a');
+    array_pop($vil_list);
+    break;
+  default:
+    echo 'ERROR: undefined country ID.';
+    exit;
 }
 
+//ファイルを新たに作って書き込む
+$fp = fopen('list_'.$country.'.txt','w+');
 
-foreach($rawVilList as $item)
+if(flock($fp,LOCK_EX))
 {
-  //村名取得 village[0]=村番号、village[1]=村名
-  $village = explode(" ",$item->plaintext);
-  if($c === 'g')
+  foreach($vil_list as $item)
   {
-    $village[0] = mb_substr($village[0],1);
-  }
-  //村プロローグURL取得
-  if($c === 'old2')
-  {
-    $proURL = $url_vil.'index.rb?vid='.$village[0].'&meslog='.$village[0].'_ready_0';
-  } else {
-    $proURL = $url_vil.$item->href; 
-  }
-  echo $village[0].','.$village[1].','.$proURL.PHP_EOL;
+    //村名取得 village[0]=村番号、village[1]=村名
+    $village = explode(" ",$item->plaintext);
 
+    switch($country)
+    {
+      case N_OLD1:
+        //日付がURLになっているため、プロローグではなく終了ページのURLを挿入
+        $pro_URL = $url.'index.rb?vid='.$village[0];
+        break;
+      case N_OLD2:
+        $proURL = $url.'index.rb?vid='.$village[0].'&meslog='.$village[0].'_ready_0';
+        break;
+      case N_MAIN:
+      case N_A:
+      case N_B:
+      case N_C:
+      case N_D:
+      case N_E:
+      case N_F:
+      case N_G:
+        $url = preg_replace("/index\.rb\?cmd=log/","",$url);
+        $proURL = $url.$item->href;
+        if($country === N_G)
+        {
+          $village[0] = mb_substr($village[0],1);
+        }
+        break;
+    }
+    fwrite($fp,$village[0].','.$village[1].','.$proURL.PHP_EOL);
+  }
+  echo 'success.';
 }
-
-$html->clear();
-unset($html);
+else
+{
+  echo 'ERROR: cannot lock file.';
+  exit;
+}
+fclose($fp);
