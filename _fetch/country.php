1e103f3bb94a5f713e1c9a2f06c82e96a1f2a949
fortmorst <fortmorst@server.fake>
2013-09-18 19:59:57 +0900

    URLも別ファイルに

--- _fetch/country.php
+++ _fetch/country.php
@@ -11,3 +11,18 @@ define('N_F','8');
 define('N_G','9');
 define('N_OLD2','10');
 define('GUTA','11');
+
+$URL_LIST = array(
+  "",
+  "http://ninjinix.x0.com/wolf_old/",
+  "http://ninjinix.x0.com/wolf0/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfa/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfb/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfc/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfd/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolfe/index.rb?cmd=log",
+  "http://ninjin002.x0.com/wolff/index.rb?cmd=log",
+  "http://wolfg.x0.com/index.rb?cmd=log",
+  "http://ninjinix.x0.com/wolf/",
+  "http://www3.marimo.or.jp/~fgmaster/cabala/sow.cgi?cmd=oldlog"
+);
