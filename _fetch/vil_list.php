a92bdefe0cfba3c8c1ae53663d22579127e3e7fd
fortmorst <fortmorst@server.fake>
2013-09-18 19:48:43 +0900

    国名を別ファイルに

--- _fetch/vil_list.php
+++ _fetch/vil_list.php
@@ -1,17 +1,6 @@
-<?
+<?php
 require_once('../../lib/simple_html_dom.php');
-
-define('N_OLD1','1');
-define('N_MAIN','2');
-define('N_A','3');
-define('N_B','4');
-define('N_C','5');
-define('N_D','6');
-define('N_E','7');
-define('N_F','8');
-define('N_G','9');
-define('N_OLD2','10');
-define('GUTA','11');
+require_once('./country.php');
 
 $URL_LIST = array(
   "",
