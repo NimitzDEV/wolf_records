a4a5ddf19e62255704582f53058584949694662c
fortmorst <fortmorst@server.fake>
2013-09-26 12:35:22 +0900

    処理完成

--- _fetch/fetch_village.php
+++ _fetch/fetch_village.php
@@ -5,16 +5,19 @@ require_once('./country.php');
 class Fetch_Village
 {
   //陣営 team
-  const TM_NONE      = 0; //陣営なし
-  const TM_VILLAGER  = 1; //村人陣営
-  const TM_WOLF      = 2; //人狼陣営
-  const TM_FAIRY     = 3; //妖魔陣営
-  const TM_LOVERS    = 4; //恋人陣営
-  const TM_LWOLF     = 6; //一匹狼陣営
-  const TM_PIPER     = 7; //笛吹き陣営
-  const TM_EFB       = 8; //邪気陣営
-  const TM_EVIL      = 9; //裏切りの陣営
-  const TM_FISH      = 10;//据え膳
+  const TM_RP        =  0; //勝敗なし
+  const TM_VILLAGER  =  1; //村人陣営
+  const TM_WOLF      =  2; //人狼陣営
+  const TM_FAIRY     =  3; //妖魔陣営
+  const TM_LOVERS    =  4; //恋人陣営
+  const TM_LWOLF     =  6; //一匹狼陣営
+  const TM_PIPER     =  7; //笛吹き陣営
+  const TM_EFB       =  8; //邪気陣営
+  const TM_EVIL      =  9; //裏切りの陣営
+  const TM_FISH      = 10; //据え膳
+  const TM_NONE      = 11; //陣営なし
+  const TM_ONLOOKER  = 12; //見物人  
+    
 
   //編成 regulation
   const RGL_C    =  1;      //C編成
@@ -27,21 +30,112 @@ class Fetch_Village
   const RGL_S_C3 =  8;      //少人数狼3
   const RGL_S_L0 =  9;      //少人数狂人なし
   const RGL_W2   = 10;      //狼2共有あり
+  const RGL_C_ST = 25;      //聖痕入りC
+  const RGL_G_ST = 17;      //聖痕入りG
   const RGL_LEO  = 11;      //決定者入り
   const RGL_TES1 = 12;      //試験壱
   const RGL_TES2 = 13;      //試験弐
-  const RGL_MIST = 14;      //深い霧の夜
   const RGL_LOVE = 15;      //恋人入り
   const RGL_LPLY = 16;      //遊び人入り
-  const RGL_G_ST = 17;      //聖痕入りG
-  const RGL_ETC  = 100;     //特殊
+  const RGL_ROLLER=21;      //素村が霊能者
+  const RGL_HERO = 22;      //占い師なし
+  const RGL_EFB  = 23;      //邪気悪魔入り
+
+  const RGL_MILL = 18;      //ミラーズホロウ
+  const RGL_DEATH= 19;      //死んだら負け
+  const RGL_TA   = 20;      //Trouble☆Aliens
+  const RGL_MIST = 14;      //深い霧の夜
+  const RGL_ETC  = 24;      //特殊
+
 
   //結果 result
-  const RSL_WIN  = 1;
-  const RSL_LOSE = 2;
-  const RSL_JOIN = 3;       //参加(非ガチ村)
-  const RSL_INVALID = 4;     //無効(新議事での突然死)
+  const RSL_WIN      = 1;
+  const RSL_LOSE     = 2;
+  const RSL_JOIN     = 3;   //参加(非ガチ村)
+  const RSL_INVALID  = 4;   //無効(新議事での突然死)
   const RSL_ONLOOKER = 5;   //見物
+
+
+  //結末 destiny
+  const DES_ALIVE   = 1;  //生存
+  const DES_RETIRED = 2;  //突然死
+  const DES_HANGED  = 3;  //処刑死
+  const DES_EATEN   = 4;  //襲撃死
+  const DES_CURSED  = 5;  //呪詛死
+  const DES_DROOP   = 7;  //衰退死
+  const DES_SUICIDE = 8;  //後追死
+  const DES_FEARED  = 9;  //恐怖死 
+  const DES_ONLOOKER= 10; //見物
+  
+
+  //能力 skill
+  const SKL_VILLAGER =  1;  //村人
+  const SKL_SEER     =  2;  //占い師
+  const SKL_MEDIUM   =  3;  //霊能者
+  const SKL_HUNTER   =  4;  //狩人
+  const SKL_MASON    =  5;  //共有者
+  const SKL_STIGMA   = 11;  //聖痕者
+  const SKL_TELEPATH = 12;  //共鳴者
+  const SKL_SAGE     = 15;  //賢者
+  const SKL_SEERWIN  = 13;  //信仰占師
+  const SKL_SEERAURA = 14;  //気占師
+  const SKL_PRIEST   = 17;  //導師
+  const SKL_MEDIWIN  = 16;  //信仰霊能者
+  const SKL_NECRO    = 18;  //降霊者
+  const SKL_LINEAGE  = 24;  //狼血族
+  const SKL_CURSED   = 26;  //呪人
+  const SKL_FOLLOWER = 19;  //追従者
+  const SKL_AGITATOR = 20;  //扇動者
+  const SKL_SG       = 32;  //生贄
+  const SKL_PRINCE   = 23;  //王子様
+  const SKL_BOUNTY   = 21;  //賞金稼
+  const SKL_ALCHEMIST= 29;  //錬金術師
+  const SKL_WITCH    = 30;  //魔女
+  const SKL_GIRL     = 31;  //少女
+  const SKL_WEREDOG  = 22;  //人犬
+  const SKL_SICK     = 28;  //病人
+  const SKL_ELDER    = 33;  //長老
+  const SKL_PROPHET  = 27;  //預言者
+  const SKL_DOCTOR   = 25;  //医師
+
+  const SKL_LUNATIC  =  6;  //狂人
+  const SKL_LUNAWHS  =  8;  //C狂
+  const SKL_FANATIC  = 36;  //狂信者
+  const SKL_LUNAPATH = 35;  //叫迷狂人
+  const SKL_JAMMER   = 34;  //邪魔之民
+  const SKL_MUPPETER = 37;  //人形使い
+  const SKL_HALFWOLF = 38;  //半狼
+  const SKL_LUNASAGE = 40;  //魔術師
+  const SKL_LUNAPRI  = 39;  //魔神官
+  const SKL_SNATCH   = 60;  //宿借之民
+
+  const SKL_WOLF     =  7;  //人狼
+  const SKL_HEADLESS = 41;  //首無騎士
+  const SKL_WISEWOLF = 42;  //智狼
+  const SKL_CURSEWOLF= 43;  //呪狼
+  const SKL_WHITEWOLF= 44;  //白狼
+  const SKL_CHILDWOLF= 45;  //仔狼
+  const SKL_DYINGWOLF= 46;  //衰狼
+  const SKL_SILENT   = 47;  //黙狼
+
+  const SKL_FAIRY    =  9;  //妖魔
+  const SKL_CHAOS    = 61;  //天魔
+  const SKL_PIXY     = 50;  //悪戯妖精
+  const SKL_MIMIC    = 48;  //擬狼妖精
+  const SKL_SNOW     = 49;  //風花妖精
+
+  const SKL_QP       = 52;  //QP
+  const SKL_PASSION  = 53;  //片想い
+  const SKL_PLAYBOY  = 59;  //遊び人
+
+  const SKL_EFB      = 51;  //邪気悪魔
+  const SKL_LWOLF    = 56;  //一匹狼
+  const SKL_PIPER    = 57;  //笛吹き
+  const SKL_FISH     = 58;  //鱗魚人
+  const SKL_PUPIL    = 54;  //弟子
+  const SKL_THIEF    = 55;  //盗賊
+  const SKL_ONLOOKER = 10;  //見物人
+
 
   //テキスト分割行
   const LIMIT = 400;
@@ -50,6 +144,7 @@ class Fetch_Village
         , $fp_village
         , $fp_users
         , $count_v
+        , $count_u
         , $lastline_v
         , $nop
         , $loop_village
@@ -109,21 +204,22 @@ class Fetch_Village
 
   function write_list($type,$array,$val,$nop=0)
   {
-    $line = implode("','",$array);
+    $line = implode('","',$array);
     switch($type)
     {
       case 'village';
-        $line = "('".$this->country."','".$line."')";
-        $this->nop = $nop;
+        $line = '("'.$this->country.'","'.$line.'")';
+        $this->nop = (int)$nop;
+        $this->count_v = $val;
         if($val === $this->lastline_v)
         {
           $this->close_list($line,$type);
           return;
         }
-        $this->count_v = $val;
         break;
       case 'users';
-        $line = "('".$line."')";
+        $line = '("'.$line.'")';
+        $this->count_u++;
         if($val === $this->nop && $this->count_v === $this->lastline_v)
         {
           $this->close_list($line,$type);
@@ -132,7 +228,7 @@ class Fetch_Village
         break;
     }
 
-    if($val === $this::LIMIT * $this->{'loop_'.$type})
+    if($this->count_u === $this::LIMIT * $this->{'loop_'.$type})
     {
       //規定行を越えたら次のファイルに移動
       $this->close_list($line,$type);
@@ -140,8 +236,7 @@ class Fetch_Village
     }
     else
     {
-      $line = $line.",\n";
-      fwrite($this->{'fp_'.$type},$line);
+      fwrite($this->{'fp_'.$type},$line.",\n");
     }
   }
 
--- _fetch/guta.php
+++ _fetch/guta.php
@@ -3,47 +3,255 @@
 require_once('./fetch_village.php');
 
 define('COUNTRY',11);  //国ID
-define('VID',0);       //villageテーブルの終了済ID
+define('VID',100);       //villageテーブルの開始ID
+$GACHI = array(//お祭り騒ぎのガチリスト
+  438,437,416,388,379,
+
+);
 
 $fetch = new fetch_Village(COUNTRY);
 
 //裏切りの陣営としてカウントするレギュレーション 深い霧は別途分岐を書く
-$TM_EVIL = array($fetch::RGL_E,$fetch::RGL_ETC);
+$TM_EVIL = array($fetch::RGL_E,$fetch::RGL_EFB,$fetch::RGL_ETC);
+
 //陣営リスト
 $TM_NORMAL = array(
-      "村人"=>$fetch::TM_VILLAGER
-    , "人狼"=>$fetch::TM_WOLF
-    , "妖精"=>$fetch::TM_FAIRY
-    , "恋人"=>$fetch::TM_LOVERS
-    , "一匹"=>$fetch::TM_LWOLF
-    , "笛吹"=>$fetch::TM_PIPER
-    , "邪気"=>$fetch::TM_EFB
-    , "裏切"=>$fetch::TM_EVIL
-    , "据え"=>$fetch::TM_FISH
+   "村人"=>$fetch::TM_VILLAGER
+  ,"人狼"=>$fetch::TM_WOLF
+  ,"妖精"=>$fetch::TM_FAIRY
+  ,"恋人"=>$fetch::TM_LOVERS
+  ,"一匹"=>$fetch::TM_LWOLF
+  ,"笛吹"=>$fetch::TM_PIPER
+  ,"邪気"=>$fetch::TM_EFB
+  ,"裏切"=>$fetch::TM_EVIL
+  ,"据え"=>$fetch::TM_FISH
+  ,"勝利"=>$fetch::TM_NONE
   );
 $TM_AMBER = array(
-      "町人"=>$fetch::TM_VILLAGER
-    , "魔術"=>$fetch::TM_WOLF
-    , "琥珀"=>$fetch::TM_FAIRY
-    , "星に"=>$fetch::TM_LOVERS
-    , "星の"=>$fetch::TM_LOVERS //エピ表記
-    , "はぐ"=>$fetch::TM_LWOLF
-    , "吟遊"=>$fetch::TM_PIPER
-    , "賭博"=>$fetch::TM_EFB
-    , "不和"=>$fetch::TM_EVIL
-    , "据え"=>$fetch::TM_FISH
+   "町人"=>$fetch::TM_VILLAGER
+  ,"魔術"=>$fetch::TM_WOLF
+  ,"琥珀"=>$fetch::TM_FAIRY
+  ,"星に"=>$fetch::TM_LOVERS
+  ,"星の"=>$fetch::TM_LOVERS //エピ表記
+  ,"はぐ"=>$fetch::TM_LWOLF
+  ,"吟遊"=>$fetch::TM_PIPER
+  ,"賭博"=>$fetch::TM_EFB
+  ,"不和"=>$fetch::TM_EVIL
+  ,"据え"=>$fetch::TM_FISH
+);
+
+//役職
+//http://www3.marimo.or.jp/~fgmaster/cabala/sow.cgi?cmd=trsdiff
+$SKL_NORMAL = array(
+   "村人"=>$fetch::SKL_VILLAGER
+  ,"聖痕者"=>$fetch::SKL_STIGMA
+  ,"結社員"=>$fetch::SKL_MASON
+  ,"共有者"=>$fetch::SKL_MASON
+  ,"共鳴者"=>$fetch::SKL_TELEPATH
+  ,"占い師"=>$fetch::SKL_SEER
+  ,"信仰占師"=>$fetch::SKL_SEERWIN
+  ,"気占師"=>$fetch::SKL_SEERAURA
+  ,"賢者"=>$fetch::SKL_SAGE
+  ,"狩人"=>$fetch::SKL_HUNTER
+  ,"守護者"=>$fetch::SKL_HUNTER
+  ,"霊能者"=>$fetch::SKL_MEDIUM
+  ,"信仰霊能者"=>$fetch::SKL_MEDIWIN
+  ,"導師"=>$fetch::SKL_PRIEST
+  ,"降霊者"=>$fetch::SKL_NECRO
+  ,"追従者"=>$fetch::SKL_FOLLOWER
+  ,"煽動者"=>$fetch::SKL_AGITATOR
+  ,"賞金稼"=>$fetch::SKL_BOUNTY
+  ,"人犬"=>$fetch::SKL_WEREDOG
+  ,"王子様"=>$fetch::SKL_PRINCE
+  ,"狼血族"=>$fetch::SKL_LINEAGE
+  ,"憑かれた人"=>$fetch::SKL_LINEAGE
+  ,"医師"=>$fetch::SKL_DOCTOR
+  ,"呪人"=>$fetch::SKL_CURSED
+  ,"預言者"=>$fetch::SKL_PROPHET
+  ,"病人"=>$fetch::SKL_SICK
+  ,"錬金術師"=>$fetch::SKL_ALCHEMIST
+  ,"魔女"=>$fetch::SKL_WITCH
+  ,"少女"=>$fetch::SKL_GIRL
+  ,"生贄"=>$fetch::SKL_SG
+  ,"長老"=>$fetch::SKL_ELDER
+  ,"邪魔之民"=>$fetch::SKL_JAMMER
+  ,"念波之民"=>$fetch::SKL_CHAOS
+  ,"コウモリ人間"=>$fetch::SKL_CHAOS
+  ,"狂人"=>$fetch::SKL_LUNATIC
+  ,"狂信者"=>$fetch::SKL_FANATIC
+  ,"人形使い"=>$fetch::SKL_MUPPETER
+  ,"囁き狂人"=>$fetch::SKL_LUNAWHS
+  ,"Ｃ国狂人"=>$fetch::SKL_LUNAWHS
+  ,"半狼"=>$fetch::SKL_HALFWOLF
+  ,"魔神官"=>$fetch::SKL_LUNAPRI
+  ,"魔術師"=>$fetch::SKL_LUNASAGE
+  ,"首無騎士"=>$fetch::SKL_HEADLESS
+  ,"首なし騎士"=>$fetch::SKL_HEADLESS
+  ,"人狼"=>$fetch::SKL_WOLF
+  ,"智狼"=>$fetch::SKL_WISEWOLF
+  ,"呪狼"=>$fetch::SKL_CURSEWOLF
+  ,"白狼"=>$fetch::SKL_WHITEWOLF
+  ,"仔狼"=>$fetch::SKL_CHILDWOLF
+  ,"衰狼"=>$fetch::SKL_DYINGWOLF
+  ,"黙狼"=>$fetch::SKL_SILENT
+  ,"栗鼠妖精"=>$fetch::SKL_FAIRY
+  ,"ハムスター人間"=>$fetch::SKL_FAIRY
+  ,"擬狼妖精"=>$fetch::SKL_MIMIC
+  ,"風花妖精"=>$fetch::SKL_SNOW
+  ,"悪戯妖精"=>$fetch::SKL_PIXY
+  ,"ピクシー"=>$fetch::SKL_PIXY
+  ,"邪気悪魔"=>$fetch::SKL_EFB
+  ,"恋愛天使"=>$fetch::SKL_QP
+  ,"キューピッド"=>$fetch::SKL_QP
+  ,"片想い"=>$fetch::SKL_PASSION
+  ,"弟子"=>$fetch::SKL_PUPIL
+  ,"神話マニア"=>$fetch::SKL_PUPIL
+  ,"盗賊"=>$fetch::SKL_THIEF
+  ,"一匹狼"=>$fetch::SKL_LWOLF
+  ,"笛吹き"=>$fetch::SKL_PIPER
+  ,"鱗魚人"=>$fetch::SKL_FISH
+  ,"遊び人"=>$fetch::SKL_PLAYBOY
+);
+//ミラーズホロウ
+$SKL_MILLERS = array(
+   "村人"=>$fetch::SKL_VILLAGER
+  ,"預言者"=>$fetch::SKL_SAGE
+  ,"守護者"=>$fetch::SKL_HUNTER
+  ,"狩人"=>$fetch::SKL_BOUNTY
+  ,"魔女"=>$fetch::SKL_WITCH
+  ,"少女"=>$fetch::SKL_GIRL
+  ,"スケープゴート"=>$fetch::SKL_SG
+  ,"長老"=>$fetch::SKL_ELDER
+  ,"人狼"=>$fetch::SKL_WOLF
+  ,"キューピッド"=>$fetch::SKL_QP
+  ,"盗賊"=>$fetch::SKL_THIEF
+  ,"笛吹き"=>$fetch::SKL_PIPER
+);
+//昏き宵闇の琥珀
+$SKL_AMBER = array(
+   "町人"=>$fetch::SKL_VILLAGER
+  ,"不在証明アリ"=>$fetch::SKL_STIGMA
+  ,"刑事"=>$fetch::SKL_MASON
+  ,"警部"=>$fetch::SKL_TELEPATH
+  ,"真名探り"=>$fetch::SKL_SEER
+  ,"見鬼"=>$fetch::SKL_SEERWIN
+  ,"異能探知"=>$fetch::SKL_SEERAURA
+  ,"魔術名鑑"=>$fetch::SKL_SAGE
+  ,"護符職人"=>$fetch::SKL_HUNTER
+  ,"好事家"=>$fetch::SKL_MEDIUM
+  ,"琥珀研究科"=>$fetch::SKL_MEDIWIN
+  ,"安楽椅子探偵"=>$fetch::SKL_PRIEST
+  ,"ラヂオマニア"=>$fetch::SKL_NECRO
+  ,"怯える者"=>$fetch::SKL_FOLLOWER
+  ,"遺志を託す者"=>$fetch::SKL_AGITATOR
+  ,"呪術マニア"=>$fetch::SKL_BOUNTY
+  ,"魔力耐性者"=>$fetch::SKL_WEREDOG
+  ,"無原罪の者"=>$fetch::SKL_PRINCE
+  ,"魔力保持者"=>$fetch::SKL_LINEAGE
+  ,"癒し手"=>$fetch::SKL_DOCTOR
+  ,"呪われた者"=>$fetch::SKL_CURSED
+  ,"琥珀病患者"=>$fetch::SKL_PROPHET
+  ,"魔封じの者"=>$fetch::SKL_SICK
+  ,"魔術師殺し"=>$fetch::SKL_ALCHEMIST
+  ,"呪薬師"=>$fetch::SKL_WITCH
+  ,"夢歩き"=>$fetch::SKL_GIRL
+  ,"怨霊憑き"=>$fetch::SKL_SG
+  ,"強き魔封じの者"=>$fetch::SKL_ELDER
+  ,"真名隠し"=>$fetch::SKL_JAMMER
+  ,"成り代わり"=>$fetch::SKL_SNATCH
+  ,"念話術師"=>$fetch::SKL_CHAOS
+  ,"悪徳琥珀商人"=>$fetch::SKL_LUNATIC
+  ,"魔術師を目撃した者"=>$fetch::SKL_FANATIC
+  ,"死人遣い"=>$fetch::SKL_MUPPETER
+  ,"魔術師の愛弟子"=>$fetch::SKL_LUNAWHS
+  ,"見習い魔術師"=>$fetch::SKL_HALFWOLF
+  ,"故売屋"=>$fetch::SKL_LUNAPRI
+  ,"見通す目"=>$fetch::SKL_LUNASAGE
+  ,"欲深き魔術師"=>$fetch::SKL_HEADLESS
+  ,"魔術師"=>$fetch::SKL_WOLF
+  ,"秘術師"=>$fetch::SKL_WISEWOLF
+  ,"呪術師"=>$fetch::SKL_CURSEWOLF
+  ,"魔導師"=>$fetch::SKL_WHITEWOLF
+  ,"付与魔術師"=>$fetch::SKL_CHILDWOLF
+  ,"瀕死の魔術師"=>$fetch::SKL_DYINGWOLF
+  ,"新人魔術師"=>$fetch::SKL_SILENT
+  ,"琥珀妖精"=>$fetch::SKL_FAIRY
+  ,"欺く琥珀妖精"=>$fetch::SKL_MIMIC
+  ,"年老いた妖精"=>$fetch::SKL_SNOW
+  ,"悪意ある妖精"=>$fetch::SKL_PIXY
+  ,"賭博屋"=>$fetch::SKL_EFB
+  ,"占星術師"=>$fetch::SKL_QP
+  ,"懸想する者"=>$fetch::SKL_PASSION
+  ,"物真似師"=>$fetch::SKL_PUPIL
+  ,"生ける屍"=>$fetch::SKL_THIEF
+  ,"はぐれ魔術師"=>$fetch::SKL_LWOLF
+  ,"吟遊詩人"=>$fetch::SKL_PIPER
+  ,"人魚"=>$fetch::SKL_FISH
+  ,"占星術フリーク"=>$fetch::SKL_PLAYBOY
 );
 
 //レギュレーション
 $RGL = array(
-    '標準'=>$fetch::RGL_LEO
-  , '深い霧の森'=>$fetch::RGL_MIST
-  , '人狼BBS C国'=>$fetch::RGL_C
-  , '人狼BBS F国'=>$fetch::RGL_F
-  , '人狼BBS G国'=>$fetch::RGL_G
-  , '人狼審問 試験壱型'=>$fetch::RGL_TES1
-  , '人狼審問 試験弐型'=>$fetch::RGL_TES2
-  , '自由設定'=>$fetch::RGL_ETC
+   '標準'=>$fetch::RGL_LEO
+  ,'新標準'=>$fetch::RGL_LEO
+  ,'深い霧の夜'=>$fetch::RGL_MIST
+  ,'人狼BBS C国'=>$fetch::RGL_C
+  ,'人狼BBS F国'=>$fetch::RGL_F
+  ,'人狼BBS G国'=>$fetch::RGL_G
+  ,'人狼審問 試験壱型'=>$fetch::RGL_TES1
+  ,'人狼審問 試験弐型'=>$fetch::RGL_TES2
+  ,'自由設定'=>$fetch::RGL_ETC
+);
+//特殊ルール
+$RGL_SP = array(
+  'ミラーズホロウ'=>$fetch::RGL_MILL
+  ,'死んだら負け'=>$fetch::RGL_DEATH
+  ,'Trouble☆Aliens'=>$fetch::RGL_TA
+  ,'深い霧の夜'=>$fetch::RGL_MIST
+);
+//自由設定でも特殊レギュにしない編成
+$RGL_FREE = array(
+    '村人x6 占い師x1 霊能者x1 人狼x2 '=>$fetch::RGL_S_2
+   ,'村人x5 占い師x1 狩人x1 霊能者x1 狂人x1 人狼x2 '=>$fetch::RGL_S_2
+   ,'村人x8 聖痕者x1 占い師x1 狩人x1 霊能者x1 囁き狂人x1 人狼x3 '=>$fetch::RGL_C_ST
+   ,'村人x8 聖痕者x1 占い師x1 守護者x1 霊能者x1 囁き狂人x1 人狼x3 '=>$fetch::RGL_C_ST
+   ,'村人x8 聖痕者x1 占い師x1 守護者x1 霊能者x1 Ｃ国狂人x1 人狼x3 '=>$fetch::RGL_C_ST
+   ,'村人x8 占い師x1 狩人x1 霊能者x1 狂人x1 人狼x3 ハムスター人間x1 '=>$fetch::RGL_E
+   ,'村人x6 占い師x1 狩人x1 霊能者x1 狂人x1 人狼x2 ハムスター人間x1 '=>$fetch::RGL_E
+   ,'村人x5 狂信者x1 人狼x1 '=>$fetch::RGL_HERO
+   ,'村人x4 狂信者x1 人狼x1 '=>$fetch::RGL_HERO
+   ,'村人x4 占い師x1 霊能者x1 人狼x2 遊び人x1 '=>$fetch::RGL_LPLY
+   ,'村人x5 占い師x1 守護者x1 霊能者x1 狂人x1 人狼x2 遊び人x1 '=>$fetch::RGL_LPLY
+   ,'村人x4 占い師x1 守護者x1 霊能者x1 狂人x1 人狼x3 遊び人x1 '=>$fetch::RGL_LPLY
+   ,'村人,占い師,霊能者,狂人,人狼,遊び人,'=>$fetch::RGL_LPLY
+);
+
+//結末
+$DES_NORMAL = array(
+   "生存者"=>$fetch::DES_ALIVE
+  ,"突然死"=>$fetch::DES_RETIRED
+  ,"処刑死"=>$fetch::DES_HANGED
+  ,"襲撃死"=>$fetch::DES_EATEN
+  ,"呪詛死"=>$fetch::DES_CURSED
+  ,"衰退死"=>$fetch::DES_DROOP
+  ,"後追死"=>$fetch::DES_SUICIDE
+  ,"恐怖死"=>$fetch::DES_FEARED
+);
+$DES_AMBER = array(
+   "生存者"=>$fetch::DES_ALIVE
+  ,"突然死"=>$fetch::DES_RETIRED//不明
+  ,"殺害"=>$fetch::DES_HANGED
+  ,"琥珀化"=>$fetch::DES_EATEN
+  ,"呪詛死"=>$fetch::DES_CURSED //そのまま
+  ,"衰退死"=>$fetch::DES_DROOP  //不明
+  ,"後追死"=>$fetch::DES_SUICIDE//不明
+  ,"恐怖死"=>$fetch::DES_FEARED//不明
+);
+
+$RSL = array(
+   "勝利"=>$fetch::RSL_WIN
+  ,"敗北"=>$fetch::RSL_LOSE
+  ,""=>$fetch::RSL_INVALID //無効(突然死)
 );
 
 $base_list = $fetch->read_list();
@@ -59,7 +267,7 @@ foreach($base_list as $val_vil=>$item_vil)
             ,'name' =>$item_vil[1]
             ,'date' =>""
             ,'nop'  =>$item_vil[2]
-            ,'rglid'=>$RGL[$item_vil[5]]
+            ,'rglid'=>""
             ,'days' =>$item_vil[4]
             ,'wtmid'=>""
   );
@@ -67,21 +275,76 @@ foreach($base_list as $val_vil=>$item_vil)
   //情報欄取得
   $html = $fetch->fetch_url($item_vil[6]);
 
-  $wtmid = $html->find('p.multicolumn_left',1)->plaintext;
-  //ガチ村のみ勝利陣営を挿入
-  if($wtmid === 'ガチ推理' OR $wtmid === '推理＆RP')
+  //レギュレーション挿入
+  $rglid= trim($html->find('dl.mes_text_report dt',1)->plaintext);
+  if(array_key_exists($rglid,$RGL_SP))
   {
-    $village['wtmid'] = $TM_NORMAL[mb_substr($item_vil[3],0,2)];
+    //特殊ルールがあるならレギュレーション扱いで挿入する
+    $village['rglid'] = $RGL_SP[$rglid];
+    echo "#".$village['vno'].' is '.$rglid.".Should check evil team.##";
+  }
+  else if($item_vil[5] === '自由設定')
+  {
+    //自由設定でも特定の編成はレギュレーションを指定する
+    //$free = preg_replace("/x\d+ /",",",$html->find('dl.mes_text_report dd',3)->plaintext);
+    $free = $html->find('dl.mes_text_report dd',3)->plaintext;
+    if(array_key_exists($free,$RGL_FREE))
+    {
+      $village['rglid'] = $RGL_FREE[$free];
+    }
+    else
+    {
+      echo "#".$village['vno'].' has '.$free."#";
+      $village['rglid'] = $RGL[$item_vil[5]];
+    }
   }
   else
   {
-    $village['wtmid'] = $fetch::TM_NONE;
+    $village['rglid'] = $RGL[$item_vil[5]];
   }
 
-  //ガチ村の自由設定は手動で修正する
-  if($village['wtmid'] !== 0 && $village['rglid'] === $fetch::RGL_ETC)
+  //言い換え
+  $rp= trim($html->find('dl.mes_text_report dt',0)->plaintext);
+  //ガチ村のみ勝利陣営を挿入
+  $wtmid = $html->find('p.multicolumn_left',1)->plaintext;
+  switch($wtmid)
   {
-    echo $village['vno'].' is 自由設定 && ガチ'.PHP_EOL;
+    case "ガチ推理":
+    case "推理＆RP":
+      switch($rp)
+      {
+      case "昏き宵闇の琥珀":
+        $village['wtmid'] = $TM_AMBER[mb_substr($item_vil[3],0,2)];
+        break;
+      default:
+        $village['wtmid'] = $TM_NORMAL[mb_substr($item_vil[3],0,2)];
+        break;
+      }
+      break;
+    case "お祭り騒ぎ":
+      if(in_array($village['vno'],$GACHI))
+      {
+        $village['wtmid'] = $TM_NORMAL[mb_substr($item_vil[3],0,2)];
+        break;
+      }
+      else
+      {
+        echo '#'.$village['vno'].'. '.$village['name'].' is お祭り騒ぎ#';
+      }
+    default:
+      $village['wtmid'] = $fetch::TM_RP;
+      break;
+  }
+
+  //狂人は裏切りの陣営かどうか
+  //ルールではなく編成が深い霧の夜なら人数によって可変
+  if(in_array($village['rglid'],$TM_EVIL) || ($item_vil[5] === "深い霧の夜" && ($village['nop'] <8 || $village['nop'] >18)))
+  {
+    $is_evil = true;
+  }
+  else
+  {
+    $is_evil = false;
   }
 
   //初日取得
@@ -90,64 +353,123 @@ foreach($base_list as $val_vil=>$item_vil)
   $html = $fetch->fetch_url($url);
   $village['date'] = mb_substr($html->find('p.mes_date',0)->plaintext,5,10);
 
-  //村を書き込む
-  $fetch->write_list('village',$village,$val_vil+1,$village['nop']);
-
   //エピローグ取得
   $html->clear();
   $url = preg_replace("/0&row=10/",$village['days']."&row=50",$url);
   $html = $fetch->fetch_url($url);
   $cast = $html->find('tbody tr.i_active');
+  
+  //村を書き込む
+  $fetch->write_list('village',$village,$val_vil+1,count($cast));
 
-  foreach($cast as $val_cast => $item_cast)
-  {
-    $users = array(
-               'vid'    =>$val_vil + VID
-              ,'persona'=>trim($item_cast->find("td",0)->plaintext)
-              ,'player' =>trim($item_cast->find("td",1)->plaintext)
-              ,'role'  =>""
-              ,'dtid'=>""
-              ,'end' =>""
-              ,'sklid'=>""
-              ,'tmid'=>""
-              ,'life'=>""
-              ,'rltid'=>""
-    );
+  //foreach($cast as $val_cast => $item_cast)
+  //{
+    //$users = array(
+               //'vid'    =>$val_vil + VID
+              //,'persona'=>trim($item_cast->find("td",0)->plaintext)
+              //,'player' =>trim($item_cast->find("td",1)->plaintext)
+              //,'role'  =>""
+              //,'dtid'=>""
+              //,'end' =>""
+              //,'sklid'=>""
+              //,'tmid'=>""
+              //,'life'=>""
+              //,'rltid'=>""
+    //);
+    
+    //$result = $item_cast->find("td",3)->plaintext;
+    //$result = mb_substr($result,0,mb_strpos($result,"\n")-1);
+    //$result = explode(' ',$result);
 
+    ////陣営と役職を取得
+    //$users['role'] = mb_substr($result[2],mb_strpos($result[2],'：')+1);
 
-    //日数
-    $users['end'] = (int)preg_replace("/(.+)日/","$1",$item_cast->find("td",2)->plaintext);
-    if($users['end'] === 0)
-    {
-      $users['end'] === $village['days'];
-    }
+    //if(mb_substr($users['role'],-2) === "居た")
+    //{
+      ////見物人設定
+      //$users['role'] = '見物人';
+      //$users['dtid'] = $fetch::DES_ONLOOKER;
+      //$users['end'] = 1;
+      //$users['sklid'] = $fetch::SKL_ONLOOKER;
+      //$users['tmid'] = $fetch::TM_ONLOOKER;
+      //$users['life'] = 0;
+      //$users['rltid'] = $fetch::RSL_ONLOOKER;
+    //}
+    //else
+    //{
+      ////日数
+      //if($result[0] === '生存者')
+      //{
+        //$users['end'] = $village['days'];
+      //}
+      //else
+      //{
+        //$users['end'] = (int)preg_replace("/(.+)日/","$1",$item_cast->find("td",2)->plaintext);
+      //}
 
-    //陣営と役職を取得
-    $role = $item_cast->find("td",4)->plaintext;
-    $role = mb_substr($role,0,mb_strpos($role,"\n")-1);
-    $users['role'] = mb_substr($role,mb_strpos($role,'：')+1);
+      ////非ガチ村は勝敗をつけずに「参加」にする
+      //if($village['wtmid'] === 0)
+      //{
+        //$users['rltid'] = $fetch::RSL_JOIN;
+      //}
+      //else
+      //{
+        //$users['rltid'] = $RSL[$result[1]];
+      //}
 
-    if(mb_substr($users['role'],-3) === "居た ")
-    {
-      //見物人設定
-      $users['role'] = '見物人';
-      $users['end'] = 0;
-      $users['life'] = 0;
-      $users['rltid'] = $fetch::RSL_ONLOOKER;
-    }
-    else
-    {
-      $users['tmid'] = $TM_NORMAL[mb_substr($role,0,2)];
-      echo $users['tmid'].'##';
-    }
+      ////役職欄に絆などついている場合
+      //if(mb_strpos($users['role'],"、") === false)
+      //{
+        //$sklid = $users['role'];
+      //}
+      //else
+      //{
+        //$sklid = mb_substr($users['role'],0,mb_strpos($users['role'],"、"));
+      //}
+      //switch($rp)
+      //{
+      //case "昏き宵闇の琥珀":
+        //$users['sklid']= $SKL_AMBER[$sklid];
+        //$users['dtid'] = $DES_AMBER[$result[0]];
+        //$users['tmid'] = $TM_AMBER[mb_substr($result[2],0,2)];
+        //break;
+      //case "ミラーズホロウ":
+        //$users['sklid']= $SKL_MILLERS[$sklid];
+        //$users['dtid'] = $DES_NORMAL[$result[0]];
+        //$users['tmid'] = $TM_NORMAL[mb_substr($result[2],0,2)];
+        //break;
+      //default:
+        //$users['sklid'] = $SKL_NORMAL[$sklid];
+        //$users['dtid'] = $DES_NORMAL[$result[0]];
+        //$users['tmid'] = $TM_NORMAL[mb_substr($result[2],0,2)];
+        //break;
+      //}
 
+      ////第三陣営がいない村では裏切りの陣営を人狼陣営扱いにする
+      //if($users['tmid'] === $fetch::TM_EVIL && $is_evil === false)
+      //{
+        //$users['tmid'] = $fetch::TM_WOLF;
+      //}
 
-      //echo "##f##".$users['persona'].$users['tmid']."##l##";
+      ////生存係数挿入
+      //if($users['dtid'] === $fetch::DES_ALIVE)
+      //{
+        //$users['life'] = 1.00;
+      //}
+      //else
+      //{
+        //$users['life'] = round(($users['end']-1) / $village['days'],2);
+      //}
 
-    $item_cast->clear();
-    unset($item_cast);
-  }
+    //}
+
+    //$fetch->write_list('users',$users,$val_cast+1);
+
+    //$item_cast->clear();
+    //unset($item_cast);
+  //}
 
   $html->clear();
   unset($html);
+  echo $village['vno']. ' is end.'.PHP_EOL;
 }
