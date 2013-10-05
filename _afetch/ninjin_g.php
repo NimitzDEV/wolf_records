67e8a0c88e4f4b03a85041d7ef521281c53c82d1
fortmorst <fortmorst@server.fake>
2013-10-05 20:19:15 +0900

    persona,player,role挿入まで

--- _afetch/ninjin_g.php
+++ _afetch/ninjin_g.php
@@ -1,386 +1,287 @@
 <?php
 
-require_once('../lib/DBAdapter.php');
 require_once('../../lib/simple_html_dom.php');
 require_once('./data.php');
+require_once('./check_village.php');
+require_once('./insert_db.php');
 
+mb_internal_encoding("UTF-8");
+
+define ("COUNTRY","ninjin_g");
+define ("CID",9);
 define ("URL_LOG","http://www.wolfg.x0.com/index.rb?cmd=log");
 define ("URL_VIL","http://www.wolfg.x0.com/index.rb?vid=");
-define ("URL_EPI","http://www.wolfg.x0.com/");
 
+$db    = new Insert_DB(CID);
+$check = new Check_Village(COUNTRY,CID,URL_VIL,URL_LOG);
+$check->check_queue();
+$check->check_fetch_vno();
+if($check->get_village())
+{
+  $fetched_v = $check->get_village();
+}
+else
+{
+  echo 'not fetch.';
+  exit;
+}
+
+$fetch = new simple_html_dom();
+$data  = new Data();
+
+$CAST_VALUE_KEY = array("persona","player","role");
 $DOPPEL = array(
    "asaki"=>"asaki&lt;G国&gt;"
   ,"motimoti"=>"motimoti&lt;G薔薇国&gt;"
 );
 
+//var_dump($fetched_v);
 
 //キャスト表の値のキー
-$cast_value_k = array("persona","player","role");
-$fetchArray = array();
-
-//
-//保留していた番号の村が終了したかチェック
-//
-$fp = fopen('q_ninjin_g.txt','r');
-
-if(flock($fp,LOCK_SH))
-{
-  $org_queue = fgets($fp);
-  $queue     = $org_queue;
-
-  if ($org_queue != null){
-    //queueの村番号を配列にする
-    $preArray= explode(',',$org_queue);
-    array_pop($preArray);
-    foreach ($preArray as $vno)
-    {
-      if (checkVilEnd($vno))
-      {
-        //終了済の村は後でqueueから消す
-        $queue = preg_replace('/'.$vno.',/',"",$queue);
-        //echo 'Queue is '.$queue.PHP_EOL;
-        $fetchArray[] = $vno;
-      } else {
-        //echo 'NOTICE: No.'.$vno.'is still proceeding.'.PHP_EOL;
-      }
-    }
-  } 
-} else {
-  echo 'ERROR: Cannot lock queue.'.PHP_EOL;
-}
-fclose($fp);
-
-
-
-//
-//新規終了村チェック
-//
-//終了した村一覧から一番上の村番号取得
-$html = new simple_html_dom();
-$html->load_file(URL_LOG);
-
-$lastNum = $html->find('a',1)->plaintext;
-$lastNum =(int) preg_replace('/G(\d+) .+/','$1',$lastNum);
-
-$html->clear;
-
-
-//DBから一番最後に取得した村番号を取得
-$pdo = connect();
-$sql = $pdo->prepare("SELECT MAX(vno) FROM village where cid=9");
-$sql->execute();
-$dbLastNum = $sql->fetch(PDO::FETCH_NUM);
-$dbLastNum = (int) $dbLastNum[0];
-$pdo = null;
-
-//取得した番号の村が終了しているかチェック
-if ($lastNum > $dbLastNum)
-{
-  $fetchNum = $lastNum - $dbLastNum;
-  for($fetchI = 1;$fetchI<=$fetchNum;$fetchI++)
-  {
-    $vno = 0;
-    $vno = $dbLastNum + $fetchI;
-
-    if (checkVilEnd($vno))
-    {
-      $fetchArray[] = $vno;
-    } else {
-      //終了していない村は一旦村番号をメモ
-      $fp = fopen('q_ninjin_g.txt','a+');
-      if(flock($fp,LOCK_SH))
-      {
-        if(fwrite($fp,$vno.','))
-        {
-          //echo 'NOTICE: '.$vno.' is proceeding. Inserted queue.'.PHP_EOL;
-        } else {
-          echo 'ERROR:'.$vno.' Cannot write queue.'.PHP_EOL;
-        }
-      } else {
-        echo 'ERROR:'.$vno.' Cannot lock queue.'.PHP_EOL;
-      }
-      fclose($fp);
-    }
-  }
-}
+//$cast_value_k = array("persona","player","role");
+//$fetchArray = array();
 
 
 //取得していない番号を取得
-if (!empty($fetchArray))
+foreach($fetched_v as $vno)
 {
-  $data = new data();
-  foreach($fetchArray as $vno)
+  //初期化
+  $village = array(
+             'cid'  =>CID
+            ,'vno'  =>$vno
+            ,'name' =>""
+            ,'date' =>""
+            ,'nop'  =>""
+            ,'rglid'=>""
+            ,'days' =>""
+            ,'wtmid'=>""
+  );
+
+  //プロローグから取得
+  $proURL = URL_VIL.$vno."&meslog=000_ready";
+  $fetch->load_file($proURL);
+
+  //ゲルト第一声から開始日時取得
+  $date = $fetch->find('div.ch1',0)->find('a',1)->name;
+  $village['date'] = date("y-m-d",preg_replace('/mes(.+)/','$1',$date));
+
+  //村名取得
+  $name = $fetch->find('title',0)->plaintext;
+  $village['name'] = preg_replace('/人狼.+\d+ (.+)/','$1',$name);
+
+
+  //エピローグ取得
+  $url_epi = preg_replace("/index\.rb\?vid=/","",URL_VIL);
+  $url_epi = $url_epi.$fetch->find('p a',-2)->href;
+
+  $fetch->clear();
+  $fetch->load_file($url_epi);
+
+  //エピが壊れている村は取得できない
+  if(!$fetch->find('div.announce'))
   {
-    //
-    //プロローグから取得
-    //
-    //プロローグのURL作成
-    $proURL = URL_VIL.$vno."&meslog=000_ready";
-    $html->load_file($proURL);
+    echo '*STOP* ERROR: VilNo.'.$vno.' epilogue is broken. log didnt save.=EOL='.PHP_EOL;
+    continue;
+  }
 
-    //ゲルト第一声から開始日時取得
-    $vilDate = $html->find('div.ch1',0)->find('a',1)->name;
-    $vilDate = preg_replace('/mes(.+)/','$1',$vilDate);
-    $vilDate = date("y-m-d",$vilDate);
+  //総日数取得
+  $village['days'] = preg_replace("/.+=0(\d{2})_party/", "$1", $url_epi) + 1;
 
-    //村名取得
-    $vilName = $html->find('title',0)->plaintext;
-    $vilName = preg_replace('/人狼.+\d+ (.+)/','$1',$vilName);
+  //名簿作成準備
+  $cast = preg_replace("/\r\n/","",$fetch->find('div.announce',-1)->plaintext);
+  //simple_html_domを抜けてきたタグを削除(IDに{}があるとbrやaが残る)
+  $cast = preg_replace(array('/<br \/>/','/<a href=[^>]+>/','/<\/a>/'),array('','',''),$cast);
+  $cast = explode('だった。',$cast);
+  //最後のスペース削除
+  array_pop($cast);
 
-    //
-    //エピローグから取得
-    //
-    //エピローグ取得
-    $epiURL = URL_EPI.$html->find('p a',-2)->href;
-    $html->load_file($epiURL);
+  //参加人数
+  $village['nop'] = count($cast);
 
-    //エピが壊れている村は取得できない
+  //人数から編成を確定
+  switch($village['nop'])
+  {
+    case 16:
+      $village['rglid'] = $data::RGL_G;
+      break;
+    case 15:
+    case 14:
+    case 13:
+      $village['rglid'] = $data::RGL_S_3;
+      break;
+    default:
+      $village['rglid']= $data::RGL_S_2;
+      break;
+  }
 
-    //.announceを取得
-    $announceEpi = $html->find('div.announce');
+  //勝敗
+  $wtmid = mb_substr($fetch->find('div.announce',-2)->plaintext,0,3);
+  switch($wtmid)
+  {
+    case '全ての': //村勝利
+      $village['wtmid'] = $data::TM_VILLAGER;
+      break;
+    case 'もう人': //狼勝利
+      $village['wtmid'] = $data::TM_WOLF;
+      break;
+    default:
+      break;
+  }
 
-    if(empty($announceEpi))
+  $cast_value = array();
+  $cast_keys = array();
+
+  foreach($cast as $item_cast)
+  {
+    //ペルソナ名、プレイヤーID、役職に分ける
+    $item_cast = preg_replace("/ ?(.+) （(.+)）、(生存|死亡)。(.+)$/", "$1#SP#$2#SP#$4", $item_cast);
+    $item_cast = explode('#SP#',$item_cast);
+
+    //得た情報にラベルがわりのキーを付ける
+    $item_cast = array_combine($CAST_VALUE_KEY,$item_cast);
+    
+    //末尾に半角スペースがある場合は、読み込めるように変換する
+    if(mb_substr($item_cast['player'],-1,1)==' ')
     {
-      echo '*STOP* ERROR: VilNo.'.$vno.' epilogue is broken. log didnt save.=EOL='.PHP_EOL;
-      continue;
+      $item_cast['player'] = preg_replace("/ /","&amp;nbsp;",$item_cast['player']);
+      echo 'ID has &nbsp:'.$item_cast['player']."=>";
     }
 
-    //突然死がエピで3人以上出ている村は注意を出す
-    $retired_count = mb_substr_count(implode($announceEpi),"突然死した","utf-8");
-    if($retired_count >= 3)
+    //重複IDがあれば変更する
+    if(array_key_exists($item_cast['player'],$DOPPEL))
     {
-      echo '*NOTICE* VilNo.'.$item[0].' is '.$retired_count.' retirer.=>';
-      //continue; データは入れておく。手動で取得する場合、キャラ名は登録済の方が楽なので
+      echo 'This ID is DOPPEL. '.$item_cast['player'].'->'.$DOPPEL[$item_cast['player']].' =>';
+      $item_cast['player'] = $DOPPEL[$item_cast['player']];
     }
 
+    //値の配列に入れる
+    $cast_value[] = $item_cast;
+    //キー用配列にペルソナ名を入れる
+    $cast_keys[] = $item_cast["persona"];
+  }
 
-    //総日数取得
-    $days = preg_replace("/.+=0(\d{2})_party/", "$1", $epiURL) + 1;
+  //ペルソナ名をキーに詳細情報を引き出す配列を作る
+  $cast = array_combine($cast_keys,$cast_value);
+  unset($item_cast,$cast_keys,$cast_value);
 
-    //名簿作成準備
-    $rawCast = null;
-    $rawCast = $html->find('div.announce',-1)->plaintext;
-    //simple_html_domを抜けてきたタグを削除(IDに{}があるとbrやaが残る/simple~要改造)
-    $modTag = array('/<br \/>/','/<a href=[^>]+>/','/<\/a>/');
-    $modTagNo = array('','','');
-    $rawCast = preg_replace($modTag,$modTagNo,$rawCast);
+  //死因と寿命を入れる配列を作成
+  //この辺あとで直す->ひとまとめの入れ子配列にする
+  //どうせ共用関数に入れる
+  $cast_retired = array();
+  $cast_hanged = array();
+  $cast_eaten = array();
 
-    //参加人数
-    $nop = count(explode('だった。',$rawCast)) -1; //最後の空白分を減らす
+  $announce = $html->find('div.announce');
 
-    //人数から編成を確定
-    switch($nop)
+  //最終日の吊り襲撃を入れる
+  sortDestiny($announce,$days,$cast_retired,$cast_hanged,$cast_eaten,$cast);
+  //進行中のURLを作成
+  $prgURL = mb_substr($proURL,0,-7,"utf-8"); //末尾の0_readyを削除(000_ready)
+  //
+  //進行中のdestinyを取得
+  //
+  for($i=1, $prgDays=$days-1; $i<=$prgDays; $i++)
+  {
+    if($i >= 10) //進行日数が二桁になる場合
     {
-      case 16:
-        $rgl = $data::RGL_G;
-        break;
-      case 15:
-      case 14:
-      case 13:
-        $rgl = $data::RGL_S_3;
-        break;
-      default:
-        $rgl = $data::RGL_S_2;
-        break;
+      $prgURL10 = mb_substr($prgURL,0,-1,"utf-8"); //末尾のゼロを削除
+      $url = $prgURL10.$i.'_progress';
+    } else{
+      $url = $prgURL.$i.'_progress';
+    }
+    $html->load_file($url);
+    $announcePrg = $html->find('div.announce');
+
+    sortDestiny($announcePrg,$i+1,$cast_retired,$cast_hanged,$cast_eaten,$cast);
+    unset($announcePrg);
+  }
+
+  //
+  //取得したdestinyとendを配列に格納する
+  //
+  makeCastDestiny($cast,$cast_retired,$data::DES_RETIRED);
+  makeCastDestiny($cast,$cast_hanged,$data::DES_HANGED);
+  makeCastDestiny($cast,$cast_eaten,$data::DES_EATEN);
+
+  //生存処理、skill,team,life,result
+  $destiny = array("destiny"=>$data::DES_ALIVE);
+  $end = array("end"=>$days);
+
+  foreach($cast as $miscItem)
+  {
+    $persona = $miscItem['persona'];
+
+    //destinyが埋まっていない人を生存とする
+    if(empty($miscItem['destiny']))
+    {
+      $cast[$miscItem['persona']] += $destiny;
+      $cast[$miscItem['persona']] += $end;
     }
 
-
-    //勝敗
-    $resAnnounce = $html->find('div.announce',-2)->plaintext;
-    $resAnnounce = mb_substr($resAnnounce,0,3,"utf-8");
-    switch($resAnnounce)
+    //役職によって能力と陣営を挿入する
+    switch($cast[$persona]['role'])
     {
-      case '全ての': //村勝利
-        $result = $data::TM_VILLAGER;
-        break;
-      case 'もう人': //狼勝利
-        $result = $data::TM_WOLF;
-        break;
-      default:
-        break;
-    }
-
-    //
-    //参加者の情報を登録
-    //
-    $rawCast = preg_replace("/\r\n/","",$rawCast);
-    $cast = explode('だった。',$rawCast);
-    //最後のスペース削除
-    array_pop($cast);
-
-    $cast_value = array();
-    $cast_keys = array();
-
-    foreach($cast as $castItem)
-    {
-      //ペルソナ名、プレイヤーID、役職に分ける
-      $castArray = explode('##SEPARATOR##',preg_replace("/ ?(.+) （(.+)）、(生存|死亡)。(.+)$/", "$1##SEPARATOR##$2##SEPARATOR##$4", $castItem));
-
-      //得た情報にラベルがわりのキーを付ける
-      $castArray = array_combine($cast_value_k,$castArray);
-      
-      //末尾に半角スペースがある場合は、読み込めるように変換する
-      if(mb_substr($castArray['player'],-1,1,"utf-8")==' ')
-      {
-        $castArray['player'] = preg_replace("/ /","&amp;nbsp;",$castArray['player']);
-        echo 'ID has &nbsp:'.$castArray['player']."=>";
-      }
-
-      //重複IDがあれば変更する
-      if(array_key_exists($castArray['player'],$DOPPEL))
-      {
-        echo 'This ID is DOPPEL. '.$castArray['player'].'->'.$DOPPEL[$castArray['player']].' =>';
-        $castArray['player'] = $DOPPEL[$castArray['player']];
-      }
-
-      //値の配列に入れる
-      $cast_value[] = $castArray;
-      //キー用配列にペルソナ名を入れる
-      $cast_keys[] = $castArray["persona"];
-    }
-
-    //ペルソナ名をキーに詳細情報を引き出す配列を作る
-    $cast = array_combine($cast_keys,$cast_value);
-    unset($castItem,$castArray,$cast_keys,$cast_value);
-
-
-    //死因と寿命を入れる配列を作成
-    //この辺あとで直す->ひとまとめの入れ子配列にする
-    //どうせ共用関数に入れる
-    $cast_retired = array();
-    $cast_hanged = array();
-    $cast_eaten = array();
-
-    $announce = $html->find('div.announce');
-
-    //最終日の吊り襲撃を入れる
-    sortDestiny($announce,$days,$cast_retired,$cast_hanged,$cast_eaten,$cast);
-    //進行中のURLを作成
-    $prgURL = mb_substr($proURL,0,-7,"utf-8"); //末尾の0_readyを削除(000_ready)
-    //
-    //進行中のdestinyを取得
-    //
-    for($i=1, $prgDays=$days-1; $i<=$prgDays; $i++)
-    {
-      if($i >= 10) //進行日数が二桁になる場合
-      {
-        $prgURL10 = mb_substr($prgURL,0,-1,"utf-8"); //末尾のゼロを削除
-        $url = $prgURL10.$i.'_progress';
-      } else{
-        $url = $prgURL.$i.'_progress';
-      }
-      $html->load_file($url);
-      $announcePrg = $html->find('div.announce');
-
-      sortDestiny($announcePrg,$i+1,$cast_retired,$cast_hanged,$cast_eaten,$cast);
-      unset($announcePrg);
-    }
-
-    //
-    //取得したdestinyとendを配列に格納する
-    //
-    makeCastDestiny($cast,$cast_retired,$data::DES_RETIRED);
-    makeCastDestiny($cast,$cast_hanged,$data::DES_HANGED);
-    makeCastDestiny($cast,$cast_eaten,$data::DES_EATEN);
-
-    //生存処理、skill,team,life,result
-    $destiny = array("destiny"=>$data::DES_ALIVE);
-    $end = array("end"=>$days);
-
-    foreach($cast as $miscItem)
-    {
-      $persona = $miscItem['persona'];
-
-      //destinyが埋まっていない人を生存とする
-      if(empty($miscItem['destiny']))
-      {
-        $cast[$miscItem['persona']] += $destiny;
-        $cast[$miscItem['persona']] += $end;
-      }
-
-      //役職によって能力と陣営を挿入する
+    case '人狼':
+      $skill = array('skill'=>$data::SKL_WOLF);
+      $team = array('team'=>$data::TM_WOLF);
+      break;
+    case '狂人':
+      $skill= array('skill'=>$data::SKL_LUNATIC);
+      $team = array('team'=>$data::TM_WOLF);
+      break;
+    default:
+      $team = array('team'=>$data::TM_VILLAGER);
       switch($cast[$persona]['role'])
       {
-      case '人狼':
-        $skill = array('skill'=>$data::SKL_WOLF);
-        $team = array('team'=>$data::TM_WOLF);
+      case '村人':
+        $skill = array('skill'=>$data::SKL_VILLAGER);
         break;
-      case '狂人':
-        $skill= array('skill'=>$data::SKL_LUNATIC);
-        $team = array('team'=>$data::TM_WOLF);
+      case '占い師':
+        $skill = array('skill'=>$data::SKL_SEER);
         break;
-      default:
-        $team = array('team'=>$data::TM_VILLAGER);
-        switch($cast[$persona]['role'])
-        {
-        case '村人':
-          $skill = array('skill'=>$data::SKL_VILLAGER);
-          break;
-        case '占い師':
-          $skill = array('skill'=>$data::SKL_SEER);
-          break;
-        case '霊能者':
-          $skill = array('skill'=>$data::SKL_MEDIUM);
-          break;
-        case '狩人':
-          $skill = array('skill'=>$data::SKL_HUNTER);
-          break;
-        }
+      case '霊能者':
+        $skill = array('skill'=>$data::SKL_MEDIUM);
+        break;
+      case '狩人':
+        $skill = array('skill'=>$data::SKL_HUNTER);
         break;
       }
-
-      $cast[$miscItem['persona']] += $skill;
-      $cast[$miscItem['persona']] += $team;
-
-      //生存係数を挿入
-      if($cast[$miscItem['persona']]['destiny'] === $data::DES_ALIVE)
-      { 
-        //生存者は1
-        $life = 1.00;
-      } else {
-        //死亡者は死亡日の前日(=生きていた日)/全日程
-        $life = round(($cast[$persona]['end']-1) / $days,2);
-      }
-      $life = array('life'=>$life);
-      $cast[$miscItem['persona']] += $life;
-
-      //勝敗を挿入
-      if($cast[$persona]['team'] === $result)
-      {
-        $castResult = array('result'=>$data::RSL_WIN);
-      } else {
-        $castResult = array('result'=>$data::RSL_LOSE);
-      }
-      $cast[$miscItem['persona']] += $castResult;
+      break;
     }
 
-    //書き込み関数に渡す配列に挿入
-    $vilList[] = array(9,$vno,$vilName,$vilDate,$nop,$rgl,$days,$result);
-    $usrList[] = $cast;
-  }
-  $html->clear;
-  unset($html);
+    $cast[$miscItem['persona']] += $skill;
+    $cast[$miscItem['persona']] += $team;
 
-
-  //
-  //バックアップを取る
-  //
-  $fp = fopen('ninjin.bak','w+');
-  if($fp)
-  {
-    if(flock($fp,LOCK_EX))
-    {
-      fwrite($fp,print_r($vilList,true));
-      fwrite($fp,print_r($usrList,true));
+    //生存係数を挿入
+    if($cast[$miscItem['persona']]['destiny'] === $data::DES_ALIVE)
+    { 
+      //生存者は1
+      $life = 1.00;
     } else {
-      echo 'ERROR: cannot lock file.';
-      fclose($fp);
-      exit;
+      //死亡者は死亡日の前日(=生きていた日)/全日程
+      $life = round(($cast[$persona]['end']-1) / $days,2);
     }
-  } else {
-    echo 'ERROR cannot open file.';
-    exit;
+    $life = array('life'=>$life);
+    $cast[$miscItem['persona']] += $life;
+
+    //勝敗を挿入
+    if($cast[$persona]['team'] === $result)
+    {
+      $castResult = array('result'=>$data::RSL_WIN);
+    } else {
+      $castResult = array('result'=>$data::RSL_LOSE);
+    }
+    $cast[$miscItem['persona']] += $castResult;
   }
+
+  //書き込み関数に渡す配列に挿入
+  $vilList[] = array(9,$vno,$vilName,$date,$nop,$rgl,$days,$result);
+  $usrList[] = $cast;
+}
+$fetch->clear;
+unset($fetch);
+exit;
+
 
   //
   //DBに入力
@@ -421,10 +322,6 @@ if (!empty($fetchArray))
     fclose($fp);
   }
 
-} else {
-  //echo 'not fetch.';
-  unset($html);
-}
 
 
 
