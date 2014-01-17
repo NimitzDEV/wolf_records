2edc8d4cb3e8c4b660f606838f083dd62f04d5a1
fortmorst <frohservice@gmail.com>
2014-01-17 23:41:33 +0900

    add:construct

--- fetch_record/country/Ning.php
+++ fetch_record/country/Ning.php
@@ -1,9 +1,29 @@
 <?php
 
-class Ning
+class Ning extends Country
 {
+  //private $fp;
+
   function __construct()
   {
-    echo 'ok';
+    $cid = 9;
+    $url_vil = "http://www.wolfg.x0.com/index.rb?vid=";
+    $url_log = "http://www.wolfg.x0.com/index.rb?vid=";
+
+    parent::__construct($cid,$url_vil,$url_log);
+    //$this->fp = fopen(__DIR__.'/../queue/9.txt','r+');
+    //if(flock($this->fp,LOCK_EX))
+    //{
+      //echo 'lock now.'.PHP_EOL;
+    //}
   }
+
+  //function write()
+  //{
+    //$string = 'okkk';
+    //var_dump(fwrite($this->fp,$string));
+    //fflush($this->fp);
+    //flock($this->fp,LOCK_UN);
+    //fclose($this->fp);
+  //}
 }
