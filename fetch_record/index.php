b2aae8a053ec39aa07e96a0174ea6ab0b7f684c9
fortmorst <fortmorst@server.fake>
2014-01-22 14:40:38 +0900

    add: 任意の国だけ取得できるように

--- fetch_record/index.php
+++ fetch_record/index.php
@@ -7,9 +7,18 @@ mb_internal_encoding("UTF-8");
 require __DIR__.'/core/ClassLoader.php';
 $class_loader = new ClassLoader([__DIR__.'/core',__DIR__.'/country',__DIR__.'/../lib']);
 
-//定数から国名リスト取得
-$countries = new ReflectionClass('Cnt');
-$countries = array_keys($countries->getConstants());
+if(isset($argv[1]))
+{
+  //引数に渡した国だけ取得
+  array_shift($argv);
+  $countries = $argv;
+}
+else
+{
+  //定数から国名リスト取得
+  $countries = new ReflectionClass('Cnt');
+  $countries = array_keys($countries->getConstants());
+}
 
 foreach($countries as $ctry)
 {
