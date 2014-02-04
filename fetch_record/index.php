ca3539576a1fa9f8d72135acfd7044a59131a731
fortmorst <fortmorst@server.fake>
2014-02-04 15:17:21 +0900

    fix: ClassLoaderを共有化

--- fetch_record/index.php
+++ fetch_record/index.php
@@ -6,7 +6,7 @@ mb_internal_encoding("UTF-8");
 mb_regex_encoding('UTF-8');
 
 //autoloader読込
-require __DIR__.'/core/ClassLoader.php';
+require '../lib/ClassLoader.php';
 $class_loader = new ClassLoader([__DIR__.'/core',__DIR__.'/country',__DIR__.'/../lib']);
 
 if(isset($argv[1]))
