8a44eaecb440d7da1b6f3f4fb164556e5b7a0d89
fortmorst <fortmorst@server.fake>
2014-02-26 17:22:58 +0900

    fix: 関数を仕様に合わせて変更

--- fetch_record/country/Silence.php
+++ fetch_record/country/Silence.php
@@ -1,6 +1,6 @@
 <?php
 
-class Real extends Country
+class Silence extends Country
 {
   use TR_SOW,AR_SOW,TR_SOW_RGL;
   function __construct()
@@ -13,5 +13,172 @@ class Real extends Country
   function fetch_policy()
   {
     $this->village->policy = true;
+  }
+  protected function fetch_name()
+  {
+    $this->village->name = $this->fetch->find('table.list tr td',1)->plaintext;
+  }
+  protected function fetch_nop()
+  {
+    $nop = $this->fetch->find('table.list tr',2)->find('td',1)->plaintext;
+    $this->village->nop = (int)preg_replace('/(\d+)人.+/','\1',$nop);
+  }
+  protected function fetch_rglid()
+  {
+    $rgl_base = trim($this->fetch->find('table.list tr',5)->find('td',1)->plaintext);
+    $rglid = preg_replace('/\r\n.+/','',$rgl_base);
+    switch($rglid)
+    {
+      case "自由設定":
+        //自由設定でも特定の編成はレギュレーションを指定する
+        $free = mb_substr($rgl_base,mb_strpos($rgl_base,'（'));
+        if(array_key_exists($free,$this->RGL_FREE))
+        {
+          $this->village->rglid = $this->RGL_FREE[$free];
+        }
+        else
+        {
+          echo $this->village->vno.' has '.$free.PHP_EOL;
+          $this->village->rglid = Data::RGL_ETC;
+        }
+        break;
+      case "標準":
+        switch(true)
+        {
+          case ($this->village->nop  >= 16):
+            $this->village->rglid = Data::RGL_F;
+            break;
+          case ($this->village->nop  === 15):
+            $this->village->rglid = Data::RGL_S_3;
+            break;
+          case ($this->village->nop <=14 && $this->village->nop >= 8):
+            $this->village->rglid = Data::RGL_S_2;
+            break;
+          default:
+            $this->village->rglid = Data::RGL_S_1;
+            break;
+        }
+        break;
+      case "ハム入り":
+        switch(true)
+        {
+          case ($this->village->nop  >= 16):
+            $this->village->rglid = Data::RGL_E;
+            break;
+          case ($this->village->nop  === 15):
+            $this->village->rglid = Data::RGL_S_3;
+            break;
+          case ($this->village->nop <=14 && $this->village->nop >= 8):
+            $this->village->rglid = Data::RGL_S_2;
+            break;
+          default:
+            $this->village->rglid = Data::RGL_S_1;
+            break;
+        }
+        break;
+      case "Ｃ国":
+        switch(true)
+        {
+          case ($this->village->nop  >= 16):
+            $this->village->rglid = Data::RGL_C;
+            break;
+          case ($this->village->nop  === 15):
+            $this->village->rglid = Data::RGL_S_C3;
+            break;
+          case ($this->village->nop <=14 && $this->village->nop >= 10):
+            $this->village->rglid = Data::RGL_S_C2;
+            break;
+          case ($this->village->nop  === 8 || $this->village->nop === 9):
+            $this->village->rglid = Data::RGL_S_2;
+            break;
+          default:
+            $this->village->rglid = Data::RGL_S_1;
+            break;
+        }
+        break;
+      case "Ｇ国":
+        switch(true)
+        {
+          case ($this->village->nop  >= 16):
+            $this->village->rglid = Data::RGL_G;
+            break;
+          case ($this->village->nop  <= 15 && $this->village->nop >= 13):
+            $this->village->rglid = Data::RGL_S_3;
+            break;
+          case ($this->village->nop <=12 && $this->village->nop >= 8):
+            $this->village->rglid = Data::RGL_S_2;
+            break;
+          default:
+            $this->village->rglid = Data::RGL_S_1;
+            break;
+        }
+        break;
+    }
+  }
+  protected function fetch_days()
+  {
+    $days = trim($this->fetch->find('p',0)->find('a',-4)->innertext);
+    $this->village->days = mb_substr($days,0,mb_strpos($days,'日')) +1;
+  }
+  protected function fetch_rp()
+  {
+    $this->village->rp = 'SOW';
+  }
+  protected function fetch_win_message()
+  {
+    $wtmid = trim($this->fetch->find('div.announce',-1)->plaintext);
+    if(preg_match("/村の更新日が延長|村の設定が変更/",$wtmid))
+    {
+      $do_i = -2;
+      do
+      {
+        $wtmid = trim($this->fetch->find('div.announce',$do_i)->plaintext);
+        $do_i--;
+      } while(preg_match("/村の更新日が延長|村の設定が変更/",$wtmid));
+    }
+    return mb_substr(preg_replace("/\r\n/","",$wtmid),-10);
+  }
+  protected function make_cast()
+  {
+    $cast = $this->fetch->find('table tr');
+    array_shift($cast);
+    $this->cast = $cast;
+  }
+  protected function fetch_from_daily($list)
+  {
+    $days = $this->village->days;
+    $rp = $this->village->rp;
+    $find = 'div.announce';
+    for($i=2; $i<=$days; $i++)
+    {
+      $announce = $this->fetch_daily_url($i,$find);
+      foreach($announce as $item)
+      {
+        $destiny = trim(preg_replace("/\r\n/",'',$item->plaintext));
+        $key= mb_substr(trim($item->plaintext),-6,6);
+        if(!isset($this->DT_NORMAL[$key]))
+        {
+          continue;
+        }
+        else
+        {
+          $persona = trim(mb_ereg_replace($this->DT_NORMAL[$key][0],'\2',$destiny,'m'));
+          $key_u = array_search($persona,$list);
+          $dtid = $this->DT_NORMAL[$key][1];
+        }
+        //妖魔陣営の無残死は呪殺死にする
+        if($this->users[$key_u]->tmid === Data::TM_FAIRY && $dtid === Data::DES_EATEN)
+        {
+          $this->users[$key_u]->dtid = Data::DES_CURSED;
+        }
+        else
+        {
+          $this->users[$key_u]->dtid = $dtid;
+        }
+        $this->users[$key_u]->end = $i;
+        $this->users[$key_u]->life = round(($i-1) / $this->village->days,2);
+      }
+      $this->fetch->clear();
+    }
   }
 }
