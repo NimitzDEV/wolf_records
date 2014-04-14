2c8a3e8a9c0aac72b75825a43221e11b76045e9f
fortmorst <fortmorst@server.fake>
2014-04-14 09:04:10 +0900

    add: doppel

--- fetch_record/country/Melon.php
+++ fetch_record/country/Melon.php
@@ -10,6 +10,7 @@ class Melon extends Country
        ,"crimson"=>"crimson&lt;瓜科&gt;"
        ,"kei"    =>"kei&lt;F瓜旧薔薇&gt;"
        ,"suzu"   =>"suzu&lt;瓜科&gt;"
+       ,"tora"   =>"tora&lt;瓜科&gt;"
        ,"uni"    =>"uni&lt;F国&gt;"
        ,"maymay" =>"maymay&lt;審問瓜&gt;"
     ];
