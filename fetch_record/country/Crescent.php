0f2e0bdbe85e328c516ae8d01293d5a8b52f184f
fortmorst <fortmorst@server.fake>
2014-05-08 14:34:44 +0900

    add: 言い換え追加

--- fetch_record/country/Crescent.php
+++ fetch_record/country/Crescent.php
@@ -3,8 +3,22 @@
 class Crescent extends Giji_Old
 {
   use AR_Guta;
-  protected $RP_SP = [
-     "ミラーズホロウ"=>'MILLERS'
+  protected $SKL_SP = 
+  [
+     "テルテル"=>Data::SKL_TERU
+    ,"求愛者"=>Data::SKL_COURTSHIP
+  ];
+  protected $TM_SP = 
+  [
+     "テルテル"=>Data::TM_TERU
+    ,"物干し"=>Data::TM_TERU
+    ,"悪霊"=>Data::TM_YANDERE
+  ];
+  protected $RP_SP = 
+  [
+     "蒼い三日月"=>'BM'
+    ,"変態BBS"=>'HENTAI'
+    ,"ＧＭイラットのクロサー"=>'CLOSED'
   ];
   function __construct()
   {
@@ -13,12 +27,13 @@ class Crescent extends Giji_Old
     $url_log = "http://www.moonpupa.jp/wolf/sow/sow.cgi?cmd=oldlog";
     parent::__construct($cid,$url_vil,$url_log);
     $this->is_evil = true;
-    //$this->SKILL = array_merge($this->SKILL,$this->SKL_GUTA);
+    $this->SKILL = array_merge($this->SKILL,$this->SKL_SP);
+    $this->TEAM = array_merge($this->TEAM,$this->TM_SP);
   }
 
   protected function fetch_nop()
   {
-    $nop = $this->fetch->find('p.multicolumn_left',7)->plaintext;
+    $nop = $this->fetch->find('p.multicolumn_left',6)->plaintext;
     $this->village->nop = (int)mb_substr($nop,0,mb_strpos($nop,'人'));
   }
   protected function check_g_rgl()
@@ -41,20 +56,51 @@ class Crescent extends Giji_Old
   }
   protected function fetch_policy()
   {
-    $policy = $this->fetch->find('p.multicolumn_left',1)->plaintext;
-    switch($policy)
+    //$policy = $this->fetch->find('p.multicolumn_left',1)->plaintext;
+    $this->village->policy = true;
+  }
+  protected function fetch_wtmid()
+  {
+    if($this->village->policy)
     {
-      case "ガチ推理（陣営勝敗最優先）":
-      case "推理＆RP（勝負しながらキャラプレイも楽しむ）":
-        $this->village->policy = true;
-        echo $this->village->vno.'.'.$this->village->name.' is guessed GACHI.'.PHP_EOL;
-        break;
-      default:
-        $this->village->policy = false;
-        echo $this->village->vno.'.'.$this->village->name.' is guessed RP.'.PHP_EOL;
-        break;
+      $wtmid = trim($this->fetch->find('p.info',-1)->plaintext);
+      if(preg_match("/村の更新日が延長されました/",$wtmid))
+      {
+        $do_i = -2;
+        do
+        {
+          $wtmid = trim($this->fetch->find('p.info',$do_i)->plaintext);
+          $do_i--;
+        } while(preg_match("/村の更新日が延長されました/",$wtmid));
+      }
+
+      //照・据え膳勝利メッセージがあったら削除
+      $wtmid = mb_ereg_replace('そして、天に召された魚料理.+|そして、お日様をたっぷり浴びた.+|明日の遠足、大丈夫かなあ。','',$wtmid,'m');
+      //特定の言い換えだけ取得文字部分を変更
+      if($this->village->rp === 'BM' || $this->village->rp === CLOSED)
+      {
+        $wtmid = mb_substr(preg_replace("/\r\n/","",$wtmid),-6);
+      }
+      else
+      {
+        $wtmid = mb_substr(preg_replace("/\r\n/","",$wtmid),2,13);
+      }
+
+      if($this->village->rp !== 'NORMAL')
+      {
+        $this->village->wtmid = $this->{'WTM_'.$this->village->rp}[$wtmid];
+      }
+      else
+      {
+        $this->village->wtmid = $this->WTM[$wtmid];
+      }
+    }
+    else
+    {
+      $this->village->wtmid = Data::TM_RP;
     }
   }
+
   protected function fetch_sklid()
   {
     $role = $this->user->role;
@@ -78,9 +124,9 @@ class Crescent extends Giji_Old
   }
   protected function fetch_tmid($result)
   {
-    if($this->village->rp === "AMBER")
+    if($this->village->rp === "HENTAI")
     {
-      $this->user->tmid = $this->TM_AMBER[mb_substr($result,0,2)];
+      $this->user->tmid = $this->TM_HENTAI[mb_substr($result,0,2)];
     }
     else
     {
@@ -88,16 +134,4 @@ class Crescent extends Giji_Old
     }
     $this->check_evil_team();
   }
-  protected function fetch_dtid($result)
-  {
-    if($this->village->rp === "AMBER")
-    {
-      $this->user->dtid = $this->DES_AMBER[$result];
-    }
-    else
-    {
-      $this->user->dtid = $this->DESTINY[$result];
-    }
-  }
-
 }
