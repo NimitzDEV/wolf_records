68ae8ad2e558acf0576a43e1930ceb23d7b6c4af
fortmorst <fortmorst@server.fake>
2014-05-09 10:19:20 +0900

    fix: キャスト表取得

--- fetch_record/country/Crescent.php
+++ fetch_record/country/Crescent.php
@@ -2,7 +2,7 @@
 
 class Crescent extends Giji_Old
 {
-  use AR_Guta;
+  use AR_Crescent;
   protected $SKL_SP = 
   [
      "テルテル"=>Data::SKL_TERU
@@ -59,17 +59,24 @@ class Crescent extends Giji_Old
     //$policy = $this->fetch->find('p.multicolumn_left',1)->plaintext;
     $this->village->policy = true;
   }
+
+  protected function fetch_date()
+  {
+    $date = $this->fetch->find('div.mes_date',0)->plaintext;
+    $date = mb_substr($date,mb_strpos($date,"2"),10);
+    $this->village->date = preg_replace('/(\d{4})\/(\d{2})\/(\d{2})/','\1-\2-\3',$date);
+  }
   protected function fetch_wtmid()
   {
     if($this->village->policy)
     {
-      $wtmid = trim($this->fetch->find('p.info',-1)->plaintext);
+      $wtmid = trim($this->fetch->find('div.info',-1)->plaintext);
       if(preg_match("/村の更新日が延長されました/",$wtmid))
       {
         $do_i = -2;
         do
         {
-          $wtmid = trim($this->fetch->find('p.info',$do_i)->plaintext);
+          $wtmid = trim($this->fetch->find('div.info',$do_i)->plaintext);
           $do_i--;
         } while(preg_match("/村の更新日が延長されました/",$wtmid));
       }
@@ -77,7 +84,7 @@ class Crescent extends Giji_Old
       //照・据え膳勝利メッセージがあったら削除
       $wtmid = mb_ereg_replace('そして、天に召された魚料理.+|そして、お日様をたっぷり浴びた.+|明日の遠足、大丈夫かなあ。','',$wtmid,'m');
       //特定の言い換えだけ取得文字部分を変更
-      if($this->village->rp === 'BM' || $this->village->rp === CLOSED)
+      if($this->village->rp === 'BM' || $this->village->rp === 'CLOSED')
       {
         $wtmid = mb_substr(preg_replace("/\r\n/","",$wtmid),-6);
       }
@@ -98,6 +105,60 @@ class Crescent extends Giji_Old
     else
     {
       $this->village->wtmid = Data::TM_RP;
+    }
+  }
+  protected function fetch_users($person)
+  {
+    $this->fetch_persona($person);
+    $this->fetch_player($person);
+
+    $role = trim($person->find('td',4)->plaintext);
+    if(mb_substr($role,-2) === '居た')
+    {
+      $this->insert_onlooker();
+    }
+    else
+    {
+      $this->fetch_role($role);
+      $this->fetch_sklid();
+      $this->fetch_dtid(trim($person->find('td',2)->plaintext));
+      $this->fetch_rltid(trim($person->find('td',3)->plaintext));
+      $this->fetch_life();
+    }
+    var_dump($this->user->get_vars());
+  }
+  protected function fetch_role($role)
+  {
+    $this->user->role = mb_ereg_replace('.+陣営：([^\r\n]+)\r\n　　.+','\\1',$role,'m');
+    $this->fetch_tmid(mb_substr($role,0,2));
+  }
+  protected function fetch_tmid($team)
+  {
+    if($this->village->rp === 'HENTAI')
+    {
+      $this->user->tmid = $this->TM_HENTAI[$team];
+    }
+    else
+    {
+      $this->user->tmid = $this->TEAM[$team];
+    }
+
+    if($this->is_evil)
+    {
+      $this->check_evil_team();
+    }
+  }
+  protected function fetch_dtid($dtid)
+  {
+    if($dtid === '生存者')
+    {
+      $this->user->end = $this->village->days;
+      $this->user->dtid = Data::DES_ALIVE;
+    }
+    else
+    {
+      $this->user->end = (int)mb_ereg_replace(".+\((\d+)d\)","\\1",$dtid,'m');
+      $this->user->dtid = $this->DESTINY[mb_substr($dtid,0,mb_strpos($dtid,"\n")-1)];
     }
   }
 
@@ -121,17 +182,5 @@ class Crescent extends Giji_Old
     {
       $this->user->sklid = $this->SKILL[$sklid];
     }
-  }
-  protected function fetch_tmid($result)
-  {
-    if($this->village->rp === "HENTAI")
-    {
-      $this->user->tmid = $this->TM_HENTAI[mb_substr($result,0,2)];
-    }
-    else
-    {
-      $this->user->tmid = $this->TEAM[mb_substr($result,0,2)];
-    }
-    $this->check_evil_team();
   }
 }
