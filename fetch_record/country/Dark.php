d4a9f654c11b386fe7c70f2737c7bdd7a0ea82b4
fortmorst <frohservice@gmail.com>
2014-06-27 03:54:46 +0900

    fix: sklidを継承

--- fetch_record/country/Dark.php
+++ fetch_record/country/Dark.php
@@ -47,29 +47,43 @@ class Dark extends SOW
     ,"憑かれた人"=>[Data::SKL_LINEAGE,Data::TM_VILLAGER]
     ,"悟られ狂人"=>[Data::SKL_SUSPECT,Data::TM_WOLF]
     ];
-  protected $SKL_ONE = [//OLD, SHIP共用
+  protected $SKL_OLD = [
      "村人"=>[Data::SKL_VILLAGER,Data::TM_VILLAGER]
-    ,"普通の人"=>[Data::SKL_VILLAGER,Data::TM_VILLAGER]
     ,"邪教徒"=>[Data::SKL_WOLF,Data::TM_WOLF]
-    ,"深きもの"=>[Data::SKL_WOLF,Data::TM_WOLF]
     ,"占い師"=>[Data::SKL_SEER,Data::TM_VILLAGER]
     ,"霊能者"=>[Data::SKL_MEDIUM,Data::TM_VILLAGER]
     ,"狂人"=>[Data::SKL_LUNATIC,Data::TM_WOLF]
     ,"守護者"=>[Data::SKL_HUNTER,Data::TM_VILLAGER]
     ,"報告者"=>[Data::SKL_MASON,Data::TM_VILLAGER]
     ,"古きもの"=>[Data::SKL_FAIRY,Data::TM_FAIRY]
-    ,"ショゴス"=>[Data::SKL_FAIRY,Data::TM_FAIRY]
     ,"入信者"=>[Data::SKL_LUNAWHS,Data::TM_WOLF]
     ,"協力者"=>[Data::SKL_STIGMA,Data::TM_VILLAGER]
     ,"狂信者"=>[Data::SKL_FANATIC,Data::TM_WOLF]
     ,"感応者"=>[Data::SKL_TELEPATH,Data::TM_VILLAGER]
     ,"古きもの・古代種"=>[Data::SKL_BAT,Data::TM_FAIRY]
-    ,"ショゴスロード"=>[Data::SKL_BAT,Data::TM_FAIRY]
     ,"醜き眷族"=>[Data::SKL_CURSEWOLF,Data::TM_WOLF]
-    ,"ダゴン"=>[Data::SKL_CURSEWOLF,Data::TM_WOLF]
     ,"忌まわしき眷族"=>[Data::SKL_WISEWOLF,Data::TM_WOLF]
-    ,"年ふりた深きもの"=>[Data::SKL_WISEWOLF,Data::TM_WOLF]
     ,"古きもの・戦闘種"=>[Data::SKL_PIXY,Data::TM_FAIRY]
+    ,"憑かれた人"=>[Data::SKL_LINEAGE,Data::TM_VILLAGER]
+    ,"悟られ狂人"=>[Data::SKL_SUSPECT,Data::TM_WOLF]
+  ];
+  protected $SKL_SHIP = [
+     "普通の人"=>[Data::SKL_VILLAGER,Data::TM_VILLAGER]
+    ,"深きもの"=>[Data::SKL_WOLF,Data::TM_WOLF]
+    ,"占い師"=>[Data::SKL_SEER,Data::TM_VILLAGER]
+    ,"霊能者"=>[Data::SKL_MEDIUM,Data::TM_VILLAGER]
+    ,"狂人"=>[Data::SKL_LUNATIC,Data::TM_WOLF]
+    ,"守護者"=>[Data::SKL_HUNTER,Data::TM_VILLAGER]
+    ,"報告者"=>[Data::SKL_MASON,Data::TM_VILLAGER]
+    ,"ショゴス"=>[Data::SKL_FAIRY,Data::TM_FAIRY]
+    ,"入信者"=>[Data::SKL_LUNAWHS,Data::TM_WOLF]
+    ,"協力者"=>[Data::SKL_STIGMA,Data::TM_VILLAGER]
+    ,"狂信者"=>[Data::SKL_FANATIC,Data::TM_WOLF]
+    ,"感応者"=>[Data::SKL_TELEPATH,Data::TM_VILLAGER]
+    ,"ショゴスロード"=>[Data::SKL_BAT,Data::TM_FAIRY]
+    ,"ダゴン"=>[Data::SKL_CURSEWOLF,Data::TM_WOLF]
+    ,"年ふりた深きもの"=>[Data::SKL_WISEWOLF,Data::TM_WOLF]
+    ,"幼生ショゴス"=>[Data::SKL_PIXY,Data::TM_FAIRY]
     ,"憑かれた人"=>[Data::SKL_LINEAGE,Data::TM_VILLAGER]
     ,"悟られ狂人"=>[Data::SKL_SUSPECT,Data::TM_WOLF]
     ];
@@ -85,7 +99,7 @@ class Dark extends SOW
     ,"面白がり"=>[Data::SKL_LUNAWHS,Data::TM_WOLF]
     ,"すご担"=>[Data::SKL_STIGMA,Data::TM_VILLAGER]
     ];
-  protected $DESTINY = [
+  protected $DT_SP = [
      '処刑された。'=>['.+(\(ランダム投票\)|投票した。)(.+) は村人達の手により処刑された。',Data::DES_HANGED]
     ,'刑された……'=>['.+(\(ランダム投票\)|投票した) ?(.+) は(村人|町の住人|村の住人)の手により処刑された……',Data::DES_HANGED]
     ,'突然死した。'=>['^( ?)(.+) は、突然死した。',Data::DES_RETIRED]
@@ -105,6 +119,7 @@ class Dark extends SOW
     $url_log = "http://o8o8.o0o0.jp/wolf/sow.cgi?cmd=oldlog";
     parent::__construct($cid,$url_vil,$url_log);
     $this->SKILL = array_merge($this->SKILL,$this->SKL_SP);
+    $this->DT_NORMAL = array_merge($this->DT_NORMAL,$this->DT_SP);
   }
   protected function fetch_from_info()
   {
@@ -179,103 +194,6 @@ class Dark extends SOW
     else
     {
       $this->user->persona = $persona;
-    }
-  }
-  protected function fetch_sklid()
-  {
-    switch($this->village->rp)
-    {
-      case 'FOOL':
-        $this->user->sklid = $this->SKL_FOOL[$this->user->role][0];
-        $this->user->tmid = $this->SKL_FOOL[$this->user->role][1];
-        break;
-      case 'OLD':
-      case 'SHIP':
-        $this->user->sklid = $this->SKL_ONE[$this->user->role][0];
-        $this->user->tmid = $this->SKL_ONE[$this->user->role][1];
-        break;
-      default:
-        $this->user->sklid = $this->SKILL[$this->user->role][0];
-        $this->user->tmid = $this->SKILL[$this->user->role][1];
-        break;
-    }
-    //呪狼の名前をメモ
-    if($this->user->sklid === Data::SKL_CURSEWOLF)
-    {
-      $this->cursewolf[] = $this->user->persona;
-    }
-  }
-  protected function fetch_from_daily($list)
-  {
-    $days = $this->village->days;
-    $rp = $this->village->rp;
-    $find = 'p.info';
-    for($i=2; $i<=$days; $i++)
-    {
-      $announce = $this->fetch_daily_url($i,$find);
-      foreach($announce as $item)
-      {
-        $destiny = trim(preg_replace("/\r\n/",'',$item->plaintext));
-        $key= mb_substr(trim($item->plaintext),-6,6);
-        if($rp === 'FOOL')
-        {
-          if(!isset($this->DT_FOOL[$key]))
-          {
-            continue;
-          }
-          else if($key === "ったみたい。")
-          {
-            echo "NOTICE: day".$i."occured EATEN but cannot find who it is.".PHP_EOL;
-            continue;
-          }
-          else
-          {
-            $persona = trim(mb_ereg_replace($this->DT_FOOL[$key][0],'\2',$destiny,'m'));
-            $key_u = array_search($persona,$list);
-            $dtid = $this->DT_FOOL[$key][1];
-          }
-        }
-        else if($rp === 'SHIP')
-        {
-          if(!isset($this->DT_SHIP[$key]))
-          {
-            continue;
-          }
-          else
-          {
-            $persona = trim(mb_ereg_replace($this->DT_SHIP[$key][0],'\2',$destiny,'m'));
-            $key_u = array_search($persona,$list);
-            $dtid = $this->DT_SHIP[$key][1];
-          }
-        }
-        else if(!isset($this->DESTINY[$key]))
-        {
-          continue;
-        }
-        else
-        {
-          $persona = trim(mb_ereg_replace($this->DESTINY[$key][0],'\2',$destiny,'m'));
-          $key_u = array_search($persona,$list);
-          $dtid = $this->DESTINY[$key][1];
-        }
-        //妖魔陣営の無残死は呪殺死にする
-        if($this->users[$key_u]->tmid === Data::TM_FAIRY && $dtid === Data::DES_EATEN)
-        {
-          $this->users[$key_u]->dtid = Data::DES_CURSED;
-        }
-        //呪狼が存在する編成で、占い師が襲撃された場合別途チェック
-        else if(!empty($this->cursewolf) && $this->users[$key_u]->sklid === Data::SKL_SEER && $dtid === Data::DES_EATEN && $this->check_cursed_seer($persona,$key_u))
-        {
-          $this->users[$key_u]->dtid = Data::DES_CURSED;
-        }
-        else
-        {
-          $this->users[$key_u]->dtid = $dtid;
-        }
-        $this->users[$key_u]->end = $i;
-        $this->users[$key_u]->life = round(($i-1) / $this->village->days,3);
-      }
-      $this->fetch->clear();
     }
   }
 }
