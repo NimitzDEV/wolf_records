1bc713349c25dab140eb2772ab17bfa8861b1336
fortmorst <frohservice@gmail.com>
2014-06-27 03:33:05 +0900

    fix: 継承可能に

--- fetch_record/country/Chitose.php
+++ fetch_record/country/Chitose.php
@@ -2,93 +2,69 @@
 
 class Chitose extends SOW
 {
-  use TRS_SOW,TRS_Chitose;
+  use TRS_SOW;
+  protected $RP_SP = [
+     'ようちえん'=>'KIDS'
+    ,'メトロポリスβ'=>'METRO'
+  ];
+  protected $WTM_KIDS= [
+     '。めでたしめでたし。'=>Data::TM_VILLAGER
+    ,'て去って行きました。'=>Data::TM_WOLF
+    ,'が残っていたのです。'=>Data::TM_FAIRY
+  ];
+  protected $WTM_METRO = [
+     '人狼に勝利したのだ！'=>Data::TM_VILLAGER
+    ,'めて去って行った……'=>Data::TM_WOLF
+    ,'くことはなかった……'=>Data::TM_FAIRY
+    ,'すすべがなかった……'=>Data::TM_FAIRY
+  ];
+  protected $DT_KIDS = [//処刑と突然死は区別するために7文字取得
+     'り眠りについた。'=>['.+(\(ランダム投票\)|投票した。)(.+) は子ども達の手により眠りについた。',Data::DES_HANGED]
+    ,'然眠りについた。'=>['^( ?)(.+) は、突然眠りについた。',Data::DES_RETIRED]
+    ,'発見された。'=>['(.+)朝、 ?(.+) が、むざん.+',Data::DES_EATEN]
+    ,'後を追った。'=>['^( ?)(.+) は(絆に引きずられるように|哀しみに暮れて) .+ の後を追った。',Data::DES_SUICIDE]
+  ];
 
   function __construct()
   {
+    $data = $this->set_village_data();
+    parent::__construct($data['cid'],$data['url_vil'],$data['url_log']);
+    $this->RP_LIST = array_merge($this->RP_LIST,$this->RP_SP);
+  }
+  function set_village_data()
+  {
+    $this->policy = true;
     $cid = 32;
     $url_vil = "http://chitose-azure.sakura.ne.jp/alf-laylah/sow/sow.cgi?vid=";
     $url_log = "http://chitose-azure.sakura.ne.jp/alf-laylah/sow/sow.cgi?cmd=oldlog";
-    $this->RP_LIST = array_merge($this->RP_LIST,$this->RP_Chitose);
-    parent::__construct($cid,$url_vil,$url_log);
-    $this->policy = true;
+    return ['cid'=>$cid,'url_vil'=>$url_vil,'url_log'=>$url_log];
   }
-  protected function fetch_from_daily($list)
+  protected function fetch_key_u($list,$rp,$item)
   {
-    $days = $this->village->days;
-    $rp = $this->village->rp;
-    $find = 'p.info';
-    for($i=2; $i<=$days; $i++)
-    {
-      $announce = $this->fetch_daily_url($i,$find);
-      foreach($announce as $item)
-      {
-        $destiny = trim(preg_replace("/\r\n/",'',$item->plaintext));
-        $key= mb_substr(trim($item->plaintext),-6,6);
-        if($rp === 'FOOL')
-        {
-          if(!isset($this->DT_FOOL[$key]))
-          {
-            continue;
-          }
-          else if($key === "ったみたい。")
-          {
-            echo "NOTICE: day".$i."occured EATEN but cannot find who it is.".PHP_EOL;
-            continue;
-          }
-          else
-          {
-            $persona = trim(mb_ereg_replace($this->DT_FOOL[$key][0],'\2',$destiny,'m'));
-            $key_u = array_search($persona,$list);
-            $dtid = $this->DT_FOOL[$key][1];
-          }
-        }
-        else if($rp === 'KIDS')
-        {
-          if($key === "りについた。")
-          {
-            $key= mb_substr(trim($item->plaintext),-8,8);
-          }
+      $destiny = trim(preg_replace("/\r\n/",'',$item->plaintext));
+      $key= mb_substr(trim($item->plaintext),-6,6);
 
-          if(!isset($this->DT_KIDS[$key]))
-          {
-            continue;
-          }
-          else
-          {
-            $persona = trim(mb_ereg_replace($this->DT_KIDS[$key][0],'\2',$destiny,'m'));
-            $key_u = array_search($persona,$list);
-            $dtid = $this->DT_KIDS[$key][1];
-          }
-        }
-        else if(!isset($this->DT_NORMAL[$key]))
-        {
-          continue;
-        }
-        else
-        {
-          $persona = trim(mb_ereg_replace($this->DT_NORMAL[$key][0],'\2',$destiny,'m'));
-          $key_u = array_search($persona,$list);
-          $dtid = $this->DT_NORMAL[$key][1];
-        }
-        //妖魔陣営の無残死は呪殺死にする
-        if($this->users[$key_u]->tmid === Data::TM_FAIRY && $dtid === Data::DES_EATEN)
-        {
-          $this->users[$key_u]->dtid = Data::DES_CURSED;
-        }
-        //呪狼が存在する編成で、占い師が襲撃した場合別途チェック
-        else if(!empty($this->cursewolf) && $this->users[$key_u]->sklid === Data::SKL_SEER && $dtid === Data::DES_EATEN && $this->check_cursed_seer($persona,$key_u))
-        {
-          $this->users[$key_u]->dtid = Data::DES_CURSED;
-        }
-        else
-        {
-          $this->users[$key_u]->dtid = $dtid;
-        }
-        $this->users[$key_u]->end = $i;
-        $this->users[$key_u]->life = round(($i-1) / $this->village->days,3);
+      if($key === "りについた。")
+      {
+        $key= mb_substr(trim($item->plaintext),-8,8);
       }
-      $this->fetch->clear();
-    }
+
+      if(!isset($this->{'DT_'.$rp}[$key]))
+      {
+        return false;
+      }
+      else
+      {
+        $persona = trim(mb_ereg_replace($this->{'DT_'.$rp}[$key][0],'\2',$destiny,'m'));
+        $dtid = $this->{'DT_'.$rp}[$key][1];
+      }
+
+      $key_u = array_search($persona,$list);
+      if($key_u === false)
+      {
+        return false;
+      }
+      $this->fetch_dtid($key_u,$dtid,$persona);
+      return $key_u;
   }
 }
