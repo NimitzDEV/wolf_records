17efb33e527224a89aa6d1417891e810b8bb4b48
fortmorst <frohservice@gmail.com>
2014-06-30 02:05:30 +0900

    add: 言い換え

--- fetch_record/country/Moon.php
+++ fetch_record/country/Moon.php
@@ -3,11 +3,27 @@
 class Moon extends SOW
 {
   use TRS_SOW;
+  protected $RP_SP = [
+     '月狼'=>'MOON'
+    ,'人狼署'=>'POLICE'
+  ];
+  protected $WTM_MOON= [
+     '。めでたしめでたし。'=>Data::TM_VILLAGER
+    ,'達の楽園なのだ――！'=>Data::TM_WOLF
+    ,'が残っていたのです。'=>Data::TM_FAIRY
+  ];
+  protected $DT_MOON = [
+     '儚く散った。'=>['.+(\(ランダム投票\)|置いた。)(.+) の命が儚く散った。',Data::DES_HANGED]
+    ,'突然死した。'=>['^( ?)(.+) は、突然死した。',Data::DES_RETIRED]
+    ,'ていた……。'=>['(.+)朝、(.+) の姿が消.+',Data::DES_EATEN]
+    ,'後を追った。'=>['^( ?)(.+) は(絆に引きずられるように|哀しみに暮れて) .+ の後を追った。',Data::DES_SUICIDE]
+  ];
   function __construct()
   {
     $cid = 56;
     $url_vil = "http://managarmr.sakura.ne.jp/sow.cgi?vid=";
     $url_log = "http://managarmr.sakura.ne.jp/sow.cgi?cmd=oldlog";
     parent::__construct($cid,$url_vil,$url_log);
+    $this->RP_LIST = array_merge($this->RP_LIST,$this->RP_SP);
   }
 }
