df573c0f79bd5bd9ce3eec3fe850aa013c31e8f6
fortmorst <frohservice@gmail.com>
2014-08-14 20:23:34 +0900

    fix: Analytics

--- result.php
+++ result.php
@@ -33,7 +33,9 @@ EOF;
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-42678919-1', 'waoon.net'); ga('require', 'displayfeatures');ga('require', 'linkid', 'linkid.js');  ga('send', 'pageview'); ga(‘set’, ‘&uid’, {{USER_ID}});
+
+  ga('create', 'UA-42678919-1', 'auto');
+  ga('send', 'pageview');
 </script>
 <!--[if lt IE 9]>
   <script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE9.js"></script>
