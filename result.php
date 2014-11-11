75a2d6a8b4d7bb876ebaaa5ce820d7563a9adf13
fortmorst <fortmorst@server.fake>
2014-11-11 16:25:53 +0900

    fix: jQueryを自サーバに

--- result.php
+++ result.php
@@ -182,7 +182,7 @@ else
       </footer>
     </div>
     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="../lib/jquery-1.11.1.min.js"></script>
     <script src="lib/checkWidth.js"></script>
     <script src="lib/dynatable.js"></script>
     <script src="lib/sorts.js"></script>
